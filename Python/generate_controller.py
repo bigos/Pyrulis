a6317555604cefbce6d0f60dee7e22e31a96edc7
Jacek Podkanski <ruby.object@googlemail.com>
2012-08-19 17:18:42 +0100

    Python: finished rails controller generator

--- Python/generate_controller.py
+++ Python/generate_controller.py
@@ -6,7 +6,7 @@ print('controller generator')
 
 def indent(i,str):
     space=' '
-    (space * i)+str
+    return (space * i)+str
 
 def guess_singular(plural):
     guessed=plural[0:-1]
@@ -31,22 +31,41 @@ def to_underscores(camel_string):
         else:
             result += letter
     return result.strip('_')
-    
+
+def spaces(n):
+    return ' ' * n   
+
 def generate(sing_camel, plu_camel):
     sing_under = to_underscores(sing_camel)
     plu_under =  to_underscores(plu_camel)
     methods = [
-        ['index','all'],             
-        ['show','find',':id'],              
-        ['new','new'],
-        ['edit','find',':id'],
+        ['index','all', False, False],             
+        ['show','find',':id', False],              
+        ['new','new', False, False],
+        ['edit','find',':id', False],
         ['create','new',":{0}".format(sing_under),"if {0}.save".format(sing_under)],              
         ['update','find',':id',"if @{0}.update_attributes(params[:{0}])".format(sing_under)],
-        ['destroy','find',':id'] ]
+        ['destroy','find',':id', False] ]
     print methods
     r = "class {0}Controller < ApplicationController\n".format(plu_camel)
     r += indent(2, "respond_to :html, :json, :xml\n")
-    # iterate here
+    for e in methods:
+        r += indent(2, "def {0}\n".format(e[0]))
+        if e[0] == 'index':
+            obj = plu_under
+        else:
+            obj = sing_under
+        r += indent(4, "@{0} = {1}.{2}".format(obj, sing_camel, e[1]))
+        if e[2]:
+            r += "(params[{0}])\n".format(e[2])
+        else:
+            r += "\n"
+        if e[3]:
+            r += indent(4,"{0}\n{2}\n{1}else\n{2}\n{1}end\n".format(e[3], spaces(4), spaces(6)))
+        if e[0] == 'destroy':
+            r += indent(4, "@{}.destroy\n".format(obj))
+        r += indent(4, "respond_with(@{0})\n".format(obj))
+        r += indent(2, "end\n")
     r += "end\n"
     print(r)
 
