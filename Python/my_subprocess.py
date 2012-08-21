6d913bd81bd89590f07f93aa3606168340b00c63
Jack <ruby.object@googlemail.com>
2012-08-21 20:18:16 +0100

    found how to get disk usage of a directory

--- Python/my_subprocess.py
+++ Python/my_subprocess.py
@@ -3,10 +3,14 @@
 
 import subprocess, os, tempfile
 
-
-
+# disk usage on mounted filesystems
 output = subprocess.Popen('df -h'.split(' '), stdout=subprocess.PIPE).communicate()[0]
-
 print ('your root partition usage:')
 print (output.split("\n"))[1]
 print
+
+
+path = '/home/jack/Programming/Pyrulis'
+
+output = subprocess.Popen(('du -sb '+path).split(' '), stdout=subprocess.PIPE).communicate()[0]
+print( "disk usage: {0} bytes".format(output.split("\t")[0]))
