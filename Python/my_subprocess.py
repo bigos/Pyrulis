1a52de582acfb23cda393b74ed0a37d76aec1a14
Jack <ruby.object@googlemail.com>
2012-08-21 20:50:05 +0100

    progress with xml generation

--- Python/my_subprocess.py
+++ Python/my_subprocess.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
     
-
 import subprocess, os, tempfile
 
 # disk usage on mounted filesystems
@@ -10,7 +9,38 @@ print (output.split("\n"))[1]
 print
 
 
+# disk usage in a directory
 path = '/home/jack/Programming/Pyrulis'
-
 output = subprocess.Popen(('du -sb '+path).split(' '), stdout=subprocess.PIPE).communicate()[0]
 print( "disk usage: {0} bytes".format(output.split("\t")[0]))
+
+
+
+from xml.etree import ElementTree
+from xml.dom import minidom
+
+top = ElementTree.Element('top')
+
+comment = ElementTree.Comment('Generated for PyMOTW')
+top.append(comment)
+
+child = ElementTree.SubElement(top, 'child')
+child.text = 'This child contains text.'
+
+child_with_tail = ElementTree.SubElement(top, 'child_with_tail')
+child_with_tail.text = 'This child has regular text.'
+child_with_tail.tail = 'And "tail" text.'
+
+child_with_entity_ref = ElementTree.SubElement(top, 'child_with_entity_ref')
+child_with_entity_ref.text = 'This & that'
+
+print ElementTree.tostring(top)
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+print(prettify (top))
