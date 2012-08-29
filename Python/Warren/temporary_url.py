5e8ce83bb2e96e96a997c710cbba8b1f42db4ce9
Jack <ruby.object@googlemail.com>
2012-08-29 14:29:10 +0100

    adding and removing alias directives works

--- Python/Warren/temporary_url.py
+++ Python/Warren/temporary_url.py
@@ -18,10 +18,19 @@ def add_temporary_url(all_nodes, top_obj, url):
     add_directive(all_nodes,top_obj, 'ServerAlias',[url])
 
 
+def remove_temporary_url(all_nodes,top_obj, url):
+    for el in all_nodes:
+        if el.name == 'ServerAlias' and el.values[0] == url:
+            parent = find_parent_obj(all_nodes,id(el))
+            parent.children.remove(el)    
+
+            
+
 
 ################################################
 add_temporary_url(all_nodes, top_obj, 'one-example-com.temporaryurl.com')
-#remove_temporary_url(all_nodes, top_obj, url)
+
+remove_temporary_url(all_nodes, top_obj, 'remove-me.example.com')
 
 print("")
 config.print_r()
