0ffaab43548c033550d9c62437f3f603b35bf2c8
Jacek Podkanski <ruby.object@googlemail.com>
2012-09-22 00:18:29 +0100

    job ordering probably works

--- Lisp/scripts/puzzle.lisp
+++ Lisp/scripts/puzzle.lisp
@@ -24,34 +24,71 @@
       (do ((line (read-line stream nil)
 		 (read-line stream nil)))
 	  ((null line))
-	(setq line (string-trim " " line))	
-	(format t "~A~%" line)
+	(setq line (string-trim " " line))		
 	(if (not (search "-" line :start1 0 :end1 1 ))	  
-	    (progn (unless (string= line "")
-		     (progn (setq x (string-trim " " (subseq line 0 (search " " line))))
-			    (setq y (string-trim " " (subseq line (+ 1 (search ">" line)))))))		
-		   (setq s (concatenate 'list  s (list (list x (if (equal y "") nil y))))))
-	    (progn
+	    (progn 
+	      (unless (string= line "")
+		(progn (setq x (string-trim " " (subseq line 0 (search " " line))))
+		       (setq y (string-trim " " (subseq line (+ 1 (search ">" line)))))))		
+	      (setq s (concatenate 'list  s (list (list x (if (equal y "") nil y))))))
+	    (progn 
 	      (setq structures (concatenate 'list structures   (list s)))
 	      (setq s nil)))))
     (setq structures (concatenate 'list structures (list s)))))
 
+(defun swap (i1 i2 lst)	    
+  (format t "~a ~a ~a~%" lst i1 i2)
+  (let* ((a1 (nth i1 lst)) (a2 (nth i2 lst)))
+    (format t " ~s ~s~%~a ~a ~a~%" a1 a2 lst i1 i2)
+    (replace lst (list a2) :start1 i1)
+    (replace lst (list a1) :start1 i2)))
+
 (let ((jobs) (job-priorities))
-  (defun job-collection (str)     
-    (format t ">>> ~A~%" str)
-    (dolist ( job-pair str)             
-      (setq jobs (concatenate 'list jobs (car job-pair)))
+  (defun job-collection (structures)     
+    (dolist ( job-pair structures)             
+      (setq jobs (concatenate 'list jobs (list (string (car job-pair)))))
       (if (cadr job-pair) 
-	  (setq job-priorities (concatenate 'list 
-					    job-priorities 
-					    (list (list (cadr job-pair) (car job-pair))))))))
+	  (setq job-priorities 
+		(concatenate 'list 
+			     job-priorities 
+			     (list (list (cadr job-pair) (car job-pair))))))))
 
   (defun job-priorities-returner ()
     (car (list job-priorities)))
 
   (defun jobs-returner () 
-    (car (list jobs))) )
+    (car (list jobs))) 
 
+  (defun ordered-jobs ()
+    (let ((cind) (pind))
+      (dolist (jp job-priorities)
+	(format t "^ ~S~%" jp)	
+	(setq cind (search (list (car jp)) jobs :test #'equalp))
+	(setq pind (search (list (cadr jp)) jobs :test #'equalp))
+	(format t "~S ! ~S + ~S ~S~%" cind pind (car jp) (cadr jp))
+	(format t "c ~A p ~A~%" cind pind)
+	(if (and cind pind)
+	    (swap cind pind jobs))
+	))) 
+
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;  def validate(str)
+;    if str == '' or str == nil
+;      raise 'No arguments given'
+;    end
+;    puts
+;    puts str
+;    # check if submitted str is valid
+;    @job_priorities.each do |jp|
+;      child_index = str.index jp[:child]
+;      parent_index = str.index jp[:parent]
+;      if child_index < parent_index
+;        return false
+;      end
+;    end
+;    return true
+;  end
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   
 (defun main ()
@@ -59,9 +96,11 @@
     (format nil "~S~%" (load-file file-path))
     (setq structures (create-structures file-path))    
     (format T "~%############################## ~S ~%" structures )
-    ;(job-collection (subseq structures 5 8))
-    ;(format T "~A~%" (job-priorities-returner))     
-    ;(format T "~A~%" (jobs-returner))
+    (job-collection (nth 3 structures))
+    (format T "~S~%" (job-priorities-returner))     
+    (format T "~S~%" (jobs-returner))
+    (format T "ordered jobs: ~S ~%"  (ordered-jobs))
+    (format T "~S~%" (jobs-returner))
     ))
 
 
