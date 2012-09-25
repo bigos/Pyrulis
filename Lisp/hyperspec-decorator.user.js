539af809895334e4e1c0f1055113bb82abd0d968
Jacek Podkanski <ruby.object@googlemail.com>
2012-09-25 22:08:32 +0100

    improved code examply styling

--- Lisp/hyperspec-decorator.user.js
+++ Lisp/hyperspec-decorator.user.js
@@ -25,18 +25,40 @@ function addGlobalStyle(css) {
 }
 
 //change <p><b> into <h3>
-var kids, obj;
-var oh, ih;
-kids = document.body.children;
-for (var k = 0; k < kids.length; k ++) {
-    obj = kids[k];
+function change_pb_to_h3(obj) {
+    var ih, oh;
     if (obj.outerHTML.match(/<p><b>.*:<\/b><\/p>/)) {
 	ih = obj.firstChild.innerHTML;
 	oh = obj.outerHTML;
-	console.log(obj.firstChild.innerHTML);
+	//console.log(ih);
 	obj.outerHTML = '<h3>'+ih+'</h3>'
     } 
 }
+
+function change_pre(obj) {
+    var n;
+    if (obj.nodeName == 'PRE') {
+	//console.log(obj.innerHTML);
+	n = obj.innerHTML.replace(/=&gt;/g, '<span class="result">=&gt;</span>');
+	obj.innerHTML = n;
+	n = obj.innerHTML.replace(/&gt;&gt;/g, '<span class="output">&gt;&gt;</span>');
+	obj.innerHTML = n;
+	//style_comments(obj);
+	//console.log(n);
+    }
+}
+
+var kids, obj ;
+kids = document.body.children;
+for (var k = 0; k < kids.length; k ++) {
+    obj = kids[k];
+    change_pb_to_h3(obj);
+    change_pre(obj);
+    //console.log(obj + ' ' + obj.innerHTML+ obj.name)
+}
+
+
+
 
 addGlobalStyle('body{padding: 1em; font-size: 16px; font-weight: 100; font-family: helvetica, verdana, sans-serif;}');
 addGlobalStyle('tt{color: #b00; font-weight: normal; font-size:1em;padding: 0.25em; font-family: monospace; }');
@@ -46,4 +68,6 @@ addGlobalStyle('pre{line-height:1.25em;border:solid #dee 1px; background:#f8fff8
 addGlobalStyle('code{line-height:1.25em; }');
 
 addGlobalStyle('h3{padding:0.25em; background: #eef; }');
-
+addGlobalStyle('.result{color:red; }');
+addGlobalStyle('.output{color:orange; }');
+addGlobalStyle('.comment{color:blue; }');
