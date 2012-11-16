9a9deeb0085c5962d22f159303ec887b4d8a5f62
Jacek Podkanski <ruby.object@googlemail.com>
2012-11-16 03:04:46 +0000

    more work on processing sgf file

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -11,5 +11,24 @@
     file-content ;;returning the value
     ))
 
-(format t "~A" (read-file-to-string *data-filename*))
+(defun get-event-list (buffer)
+  (let ((event-start) (event-end 0) (event) (all-events))
+    (loop while event-end do 			
+	 (setf event-start (position #\; buffer :start event-end))
+	 (setf event-end (position #\; buffer :start (+ event-start 1)))
+	 (setf event (subseq buffer event-start event-end ))
+	 (setf all-events  (append all-events (list event))))
+    all-events))
+(defun get-kv-list (buffer)
+  (let ((keys-list (list "B" "KO" "MN" "W" "AB" "AE" "AW" "PL" "C" "DM" "GB" "GW" "HO" "N" "UC" "V" "BM" "DO" "IT" "TE" "AR" "CR" "DD" "LB" "LN" "MA" "SL" "SQ" "TR" "AP" "CA" "FF" "GM" "ST" "SZ" "AN" "BR" "BT" "CP" "DT" "EV" "GN" "GC" "ON" "OT" "PB" "PC" "PW" "RE" "RO" "RU" "SO" "TM" "US" "WR" "WT" "BL" "OB" "OW" "WL" "FG" "PM" "VW")))
+
+))
+
+(defun main ()
+  (let* ((buffer (read-file-to-string *data-filename*)) (all-events (get-event-list buffer)) (all-kv (get-kv-list buffer)))	
+    (loop as x from 0 to 5  do
+	 (format t "~A~%" (nth x all-events)))			       
+    ))
+
+(main)
 
