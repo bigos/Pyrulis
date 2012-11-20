ea9285eec6bd300267134395a96935d979a98e28
Jacek <ruby.object@googlemail.com>
2012-11-20 00:24:10 +0000

    keys codes cleanup

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -15,8 +15,8 @@
   (list "B" "W" "C" "N" "V" 
 	"KO" "MN" "AB" "AE" "AW" "PL" "DM" "GB" "GW" "HO" "UC" "BM" 
 	"DO" "IT" "TE" "AR" "CR" "DD" "LB" "LN" "MA" "SL" "SQ" "TR" 
-	"AP" "CA" "FF" "GM" "ST" "SZ" "AN" "BR" "BT" "CP" "DT" "EV" "GN" 
-	"GC" "ON" "OT" "PB" "PC" "PW" "RE" "RO" "RU" "SO" "TM" 
+	"AP" "CA" "FF" "GM" "ST" "SZ" "AN" "BR" "BT" "CP" "DT" "EV" 
+	"GN" "GC" "ON" "OT" "PB" "PC" "PW" "RE" "RO" "RU" "SO" "TM" 
 	"US" "WR" "WT" "BL" "OB" "OW" "WL" "FG" "PM" "VW"))
 
 (defun find-key-position (buffer pos )
