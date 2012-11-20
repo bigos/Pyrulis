4b94ea0f98c1be3dac0d9dbec9839a28da1d7e4c
Jacek <ruby.object@googlemail.com>
2012-11-20 08:35:31 +0000

    got cleaner loop ending conditions

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -24,61 +24,40 @@
 
 (defun closing-bracket (buffer pos)
   (let ((last-ltr #\a) (ltr))
-    (loop while (< pos (1- (length buffer))) do
+    (loop while (< pos  (length buffer)) do
 	 (setf ltr (char buffer pos))
 	 (if (and (eq #\] ltr) (not (eq #\\ last-ltr)))
 	     (return pos))
 	 (setf last-ltr (char buffer  pos))
-	 (incf pos))
-    pos
-))
+	 (incf pos))))
 
 (defun find-key-position  (buffer pos)
-  (let ((key) (res ))
-					;(format t " ^^~a ^^^ " (subseq buffer pos (+ 5 pos)))
+  (let ((key) (res ))		 
     (dolist (el (keys-list))
-      (setf key (search el buffer :start2 pos :end2 (opening-bracket buffer pos)))
-   
-      (if key (progn (setf res key)
-		     ;;;(format t "        !!!>>!! ~a ~a " key pos)
-		     ))
-
-)
-    res
-  
-    )
-  )
+      (setf key (search el buffer :start2 pos :end2 (opening-bracket buffer pos)))   
+      (if key (progn (setf res key))))
+    res))
 
 (defun get-key-value-position (buffer pos) 
-  (let* ((key-pos (find-key-position buffer pos)) 
-	 (opb (opening-bracket buffer pos))  
-	 (clb (closing-bracket buffer pos)) (key) (val))
-
+  (let* ((key-pos) (opb) (clb) (key) (val))
     (format t "@ ~a < ~a ~A<<<<<  " pos key-pos (subseq buffer pos (+ 0 pos)))
     (setf key-pos (find-key-position buffer pos)) 
-    (format t "here 1  ")
     (setf opb (opening-bracket buffer pos))  
-    (format t "here 2  ")
     (setf clb (closing-bracket buffer pos)) 
-
-    (format t ">>> k ~a o ~a cl ~a~%" key-pos opb clb)
     (unless opb (quit))
     (setf key (subseq buffer key-pos opb)) 
-  (format t "here 3  ")
     (setf val (subseq buffer (1+ opb) clb))
-  (format t "here 4  ")
-  (list  (1+ clb) key val )
+    (list  (1+ clb) key val )
     ))
 
 (defun get-event-list (buffer)
-  (let ((event-start) (event-end 0) (all-events) (key-pos) (result))    
-    (loop while event-end do 			
-	 (setf event-start (position #\; buffer :start event-end) )
+  (let ((event-start) (event-end 0) (all-events) (key-pos 0) (result))    
+    (loop while (< key-pos (- (length buffer) 3)) do 			
+	 (setf event-start (position #\; buffer :start event-end))
 	 (setf event-end (position #\; buffer :start (1+ event-start)))
-	 (setf key-pos (find-key-position buffer event-start))
-	 (format t "~%i'm here   ~A~%" (subseq buffer event-start event-end))
-	 (loop while   (< key-pos (length buffer))  do
-	      (if key-pos  	    
+	 (format t "~%i'm here  ~A~%" (subseq buffer event-start event-end))
+	 (loop while (< key-pos (- (length buffer) 2))  do
+	      (if (closing-bracket buffer key-pos)	    
 		  (setf result (get-key-value-position buffer key-pos)))
 	      (format t "~A <<<~%" result)
 	      (setf key-pos (car result))	      
