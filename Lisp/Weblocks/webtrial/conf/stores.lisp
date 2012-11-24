d8d9f659c8e9164c034db3fcd5a4510e913e696c
Jacek Podkanski <ruby.object@googlemail.com>
2012-11-24 00:39:02 +0000

    deleted weblocks files

--- Lisp/Weblocks/webtrial/conf/stores.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :webtrial)
-
-;;; Multiple stores may be defined. The last defined store will be the
-;;; default.
-(defstore *webtrial-store* :prevalence
-  (merge-pathnames (make-pathname :directory '(:relative "data"))
-		   (asdf-system-directory :webtrial)))
-
