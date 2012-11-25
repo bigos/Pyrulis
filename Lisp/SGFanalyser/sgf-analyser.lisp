cc707799728dc56672130fde42b2acea91599bd9
Jacek <ruby.object@googlemail.com>
2012-11-25 06:28:15 +0000

    got values into lists

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -1,6 +1,6 @@
 ;;;#! /usr/bin/sbcl --script
-(defvar *sgf-files-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/sgf_files/" )
-(defvar *data-filename* (format nil "~A/~A" *sgf-files-path* "jacekpod-buonof.sgf") )
+(defvar *sgf-files-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/sgf_files" )
+(defvar *data-filename* (format nil "~A/~A" *sgf-files-path* "jacekpod-coalburner.sgf"))
 
 (defun read-file-to-string (filename)
   (let ((file-content))
@@ -22,14 +22,32 @@
   (position #\[ buffer :start pos))
 
 (defun closing-bracket (buffer pos)
-  (let ((last-ltr) (ltr) (next-ltr))    
+  (let ((last-ltr) (ltr))    
     (loop while (< pos  (length buffer)) do
-	 (setf ltr (char buffer pos))
-	 (setf next-ltr (char buffer (1+ pos)))
-	 (if (and (eq #\] ltr) (not (eq #\\ last-ltr)) (not (eq #\[ next-ltr)) )
+	 (setf ltr (char buffer pos))	 
+	 (if (and (eq #\] ltr) (not (eq #\\ last-ltr)))
 	     (return pos))
 	 (setf last-ltr (char buffer  pos))
 	 (incf pos))))
+
+(defun last-closing-bracket (buffer pos)
+  (let ((clb))    
+    (loop while (< pos  (length buffer)) do
+	 (setf clb (closing-bracket buffer pos))
+	 (unless (eq #\[ (char buffer (1+ clb)))	   
+	   (return clb))
+	 (setf pos (1+ clb)))))
+
+
+(defun val-to-list (val)
+  (let ((pos 0) (opb) (clb) (res)) 
+    (loop while (< pos (length val)) do
+	 (setf opb (opening-bracket val pos))
+	 (setf clb (closing-bracket val pos))
+	 (setf pos (1+ clb))
+	 (setf res (append res (list (subseq val (1+ opb) clb)))))
+    res))
+
 
 (defun find-key-position  (buffer pos)
   (let ((key) (res ))		 
@@ -42,22 +60,24 @@
   (let* ((key-pos) (opb) (clb) (key) (val))
     (setf key-pos (find-key-position buffer pos)) 
     (setf opb (opening-bracket buffer pos))  
-    (setf clb (closing-bracket buffer pos)) 
+    (setf clb (last-closing-bracket buffer pos)) 
     (setf key (subseq buffer key-pos opb))     
     (setf val (subseq buffer opb (1+ clb)))
     (list  (1+ clb) key val )
     ))
 
 (defun get-event-list (buffer)
-  (let ((event-start) (event-end 0) (all-events) (key-pos 0) (result))    
+  (let ((event-start) (event-end 0) (all-events) (key-pos 0) (result) (vlst))    
     (loop while (< key-pos (- (length buffer) 3)) do 			
 	 (setf event-start (position #\; buffer :start event-end))
 	 (setf event-end (position #\; buffer :start (1+ event-start)))
 	 (format t "~%i'm here  ~A~%" (subseq buffer event-start event-end))
 	 (loop while (< key-pos (- (length buffer) 3))  do	      
-	      (if (closing-bracket buffer key-pos)	    
+	      (if (last-closing-bracket buffer key-pos)	    
 		  (setf result (get-key-value-position buffer key-pos)))
-	      (format t "~S <<<~%" result)
+	      (setf vlst (val-to-list (caddr result)))
+	      ;(format t "~s  " (caddr result))
+	      (format t "~S <<< ~S~%" result vlst)
 	      (if (car result) 
 		  (setf key-pos (car result)))	      
 	      ))
@@ -66,8 +86,7 @@
 
 (defun main ()
   (let* ((buffer (read-file-to-string *data-filename*)) (all-events (get-event-list buffer)) )	
-    (loop as x from 0 to 5  do
-	 (format t "~A~%" (nth x all-events)))			       
+    			       
     ;(format T "~%~%~S <<<<<<<<<<~%" all-events)
     ))
 
