ac645f8dd7ee76a78a042f01427eedb1bd8282e0
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-01 00:32:56 +0000

    moved libraries loading code to a separate file

---
+++ Lisp/SGFanalyser/load.lisp
@@ -0,0 +1,7 @@
+(defun load-library (name)
+  (load (concatenate 'string *libraries-path* name)))
+
+(defpackage :sgf-importer 
+  (:use :common-lisp)
+  (:export :get-move-list))
+(load-library "sgf-importer.lisp")
--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -1,3 +1,7 @@
+(defvar *app-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/")
+(defvar *sgf-files-path* (concatenate 'string *app-path* "sgf_files/") )
+(defvar *sgf-data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
+(defvar *libraries-path* (concatenate 'string *app-path* "libraries/"))
 
 (defun read-file-to-string (filename)
   (let ((file-content))
@@ -6,18 +10,12 @@
       (read-sequence file-content stream ))   
     file-content ))
 
-(defvar *app-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/")
-(defvar *sgf-files-path* (concatenate 'string *app-path* "sgf_files/") )
-(defvar *data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
-(defvar *libraries-path* (concatenate 'string *app-path* "libraries/")) 
-
-(defpackage :sgf-importer 
-  (:use :common-lisp)
-  (:export :get-move-list))
-(load (concatenate 'string *libraries-path* "sgf-importer.lisp"))
+;; load libraries
+(load (concatenate 'string *app-path* "load.lisp"))
 
 (defun main ()
-  (let* ((buffer (read-file-to-string *data-filename*)) (all-moves (sgf-importer:get-move-list buffer)) )	    			       
+  (let* ((buffer (read-file-to-string *sgf-data-filename*)) 
+	 (all-moves (sgf-importer:get-move-list buffer)))	    			       
     (format T "~%~%~A <<<<<<<<<<~%" all-moves)
     ))
 
