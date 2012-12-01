1b0d39a293ea1b7919613d7cf3369ec18b8e3294
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-01 00:38:12 +0000

    more code being moved around

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -3,15 +3,16 @@
 (defvar *sgf-data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
 (defvar *libraries-path* (concatenate 'string *app-path* "libraries/"))
 
+;; load libraries
+(load (concatenate 'string *app-path* "load.lisp"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun read-file-to-string (filename)
   (let ((file-content))
     (with-open-file (stream filename)
       (setf file-content (make-string (file-length stream)))
       (read-sequence file-content stream ))   
     file-content ))
-
-;; load libraries
-(load (concatenate 'string *app-path* "load.lisp"))
 
 (defun main ()
   (let* ((buffer (read-file-to-string *sgf-data-filename*)) 
