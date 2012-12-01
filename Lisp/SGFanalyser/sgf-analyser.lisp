4d49c0cec096886ead18527bb0e8e866a3722c93
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-01 00:19:23 +0000

    created libraries directory

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -1,8 +1,4 @@
 
-
-(defvar *sgf-files-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/sgf_files/" )
-(defvar *data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
-
 (defun read-file-to-string (filename)
   (let ((file-content))
     (with-open-file (stream filename)
@@ -10,10 +6,15 @@
       (read-sequence file-content stream ))   
     file-content ))
 
+(defvar *app-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/")
+(defvar *sgf-files-path* (concatenate 'string *app-path* "sgf_files/") )
+(defvar *data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
+(defvar *libraries-path* (concatenate 'string *app-path* "libraries/")) 
+
 (defpackage :sgf-importer 
   (:use :common-lisp)
   (:export :get-move-list))
-(load "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/sgf-importer.lisp")
+(load (concatenate 'string *libraries-path* "sgf-importer.lisp"))
 
 (defun main ()
   (let* ((buffer (read-file-to-string *data-filename*)) (all-moves (sgf-importer:get-move-list buffer)) )	    			       
