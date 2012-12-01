27805d4895c0f77ba5c6afb096436771c60fd102
Jacek <ruby.object@googlemail.com>
2012-12-01 01:44:49 +0000

    more code cleanup

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -1,7 +1,7 @@
 (defvar *app-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/")
-(defvar *sgf-files-path* (concatenate 'string *app-path* "sgf_files/") )
-(defvar *sgf-data-filename* (concatenate 'string *sgf-files-path* "jacekpod-coalburner.sgf"))
-(defvar *libraries-path* (concatenate 'string *app-path* "libraries/"))
+(defvar *libraries-path*    (concatenate 'string *app-path* "libraries/"))
+(defvar *sgf-data-filename* (concatenate 'string *app-path* "sgf_files/" "jacekpod-coalburner.sgf"))
+
 
 ;; load libraries
 (load (concatenate 'string *app-path* "load.lisp"))
