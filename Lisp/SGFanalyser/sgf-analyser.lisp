a6931f9369e0c5702dc2d8850a071579ae85aa20
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-10 01:02:48 +0000

    moving info to global variables

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -5,13 +5,17 @@
 
 (in-package :sgf-analyser)
 
-;;; define global variables
+;;; define game data related global variables
 (defvar *app-path* "/home/jacek/Programming/Pyrulis/Lisp/SGFanalyser/")
 (defvar *libraries-path*    (concatenate 'string *app-path* "libraries/"))
 (defvar *sgf-data-filename* (concatenate 'string *app-path* "game_records/" "jacekpod-coalburner.sgf"))
  
 ;;; load libraries
 (load (concatenate 'string *app-path* "load.lisp"))
+
+;;; other global variables
+(defparameter *all-moves*  (sgf-importer:get-move-list *sgf-data-filename*))
+(defparameter *header-data* (car *all-moves*))
 
 (defun find-kv (kv-list key)
   (dolist (kv kv-list)
@@ -26,19 +30,20 @@
 (defun get-value (kv-list key)
   (get-v-part (find-kv kv-list key)))
 
-(defun header-info (kv-list)
-  (let ((f))
-    (dolist (el kv-list)
-      (format T "!!!> ~S~%" el))
-    (format T "~S~%"    (get-value kv-list "AB"))
-    (format T "~S~%"    (get-value kv-list "RU"))
+(defun game-stats (header-data)
+  (let ((stats "")) 
+    (concatenate 'string stats "white: " (get-value header-data "PW") " black: " (get-value header-data "PB") )
+    (concatenate 'string stats "board size: "  (get-value header-data "SZ") " rules: " (get-value header-data "RU") " result: "  (get-value header-data "RE"))
+    (format nil "~S" stats)
     ))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun main ()
-  (let* ((all-moves))  
-    (setf all-moves (sgf-importer:get-move-list *sgf-data-filename*))
-    (format T "~%~%~A <<<<<<<<<<~%" all-moves)
-    (header-info (car all-moves))
+  (let* ( (header-data))      
+    (format T "~%~%~A <<<<<<<<<<~%" *all-moves*)    
+    (format t ">>>> ~S <<<<<~%" *header-data*)
+    (format t "~S +++++++++++++++++~%" (get-value *header-data* "RE"))
+    (format t "game stats: ~S~%" (game-stats *header-data*))
     ))
 
 ;;;==================================================
