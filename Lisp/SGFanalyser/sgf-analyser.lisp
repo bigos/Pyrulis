d523872f29e285290c93a912719438751cfe8bf0
Jacek Podkanski <ruby.object@googlemail.com>
2013-02-05 12:49:10 +0000

    validation refactoring 2

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -13,7 +13,9 @@
 ;;; other global variables
 (defvar *sgf-data-filename* (concatenate 'string *app-path* "game_records/" "jacekpod-coalburner.sgf"))
 (defparameter *game-record* (sgf-importer:get-move-list *sgf-data-filename*))
+(defvar *board-size* (parse-integer (header-value "SZ")))
 (defvar *column-letters* '("a" "b" "c" "d" "e" "f" "g" "h" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t")) 
+(defvar *last-column-letter* (car (subseq *column-letters* (- (parse-integer (header-value "SZ")) 1))))
 
 (defun header-value (key)  
   (let ((kv (assoc key (car *game-record*) :test #'equalp)))
@@ -60,23 +62,17 @@
     (format t "~%~%")))
 
 (defun parse-board-coordinates (str)
-  (let* ((board-size (parse-integer (header-value "SZ")))
-	 (column (position (subseq str 0 1) (subseq *column-letters* 0 board-size) :test #'equal)) 
+  (let* ((column (position (subseq str 0 1) (subseq *column-letters* 0 *board-size*) :test #'equal)) 
 	 (row (- (parse-integer (header-value "SZ")) (parse-integer (subseq str 1)))))
     (cons column row)))
 
-(defun last-column-letter ()
-  (let ((board-size (parse-integer (header-value "SZ"))))
-    (car (subseq *column-letters* (- board-size 1) ))))
-
 (defun max-coordinate ()
-  (let ((board-size (parse-integer (header-value "SZ"))))
-    (if (> board-size 19)
-	(error "too big board size"))
-    (format nil "~a~a" (last-column-letter) board-size)))
+  (if (> *board-size* 19)
+      (error "too big board size"))
+  (format nil "~a~a" *last-column-letter* *board-size*))
 
 (defun enter-coordinates ()
-  (let ((parsed) (board-size (parse-integer (header-value "SZ"))))
+  (let ((parsed))
     (loop until (and (car parsed) (cdr parsed)) 
        do 
 	 (format t "~%~%Enter coordinates (a1 - ~A) " (max-coordinate))	 
@@ -84,23 +80,22 @@
 	     (progn
 	       (setq parsed (parse-board-coordinates (read-line)))
 	       (unless (car parsed)
-		 (format t "~&wrong column entered, you need a - ~A , except i" (last-column-letter)))
-	       (if (or (> (cdr parsed) (- board-size 1)) ;checks for correct row, max 18 in case of 19 size boad
+		 (format t "~&wrong column entered, you need a - ~A , except i" *last-column-letter*))
+	       (if (or (> (cdr parsed) (- *board-size* 1)) ;checks for correct row, max 18 in case of 19 size boad
 		       (< (cdr parsed) 0))
 		   (progn
-		     (format t "~&wrong row entered, you need something between 1 and ~A"  board-size)
+		     (format t "~&wrong row entered, you need something between 1 and ~A"  *board-size*)
 		     (setf (cdr parsed) nil))))
 	   (condition (err) (format t "couldn't parse the coordinates, enter a1 to ~a ~&raised:  ~S~&~A" (max-coordinate) err err))))
     parsed))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun main ()        
-  (let ((board) (board-size) (move) (coordinates))
+  (let ((board) (move) (coordinates))
     (format T "~%~%~A <<<<<<<<<<~%" *game-record*)   
     (game-stats )
-    (setf board-size (parse-integer (header-value "SZ")))
-    (format t "~%~d <<< board size ~%" board-size)
-    (setf board (make-array (list board-size board-size) :initial-element nil))
+    (format t "~%~d <<< board size ~%" *board-size*)
+    (setf board (make-array (list *board-size* *board-size*) :initial-element nil))
       
     ;;sample char2int
     (loop for x from (char-code #\a) to (char-code #\s) do
