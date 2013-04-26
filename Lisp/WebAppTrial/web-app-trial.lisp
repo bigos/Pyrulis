8873ffacebdce9699cc26bb41bc82195fdcf7a35
Jacek <ruby.object@googlemail.com>
2013-04-26 10:08:13 +0100

    added stopping of server

--- Lisp/WebAppTrial/web-app-trial.lisp
+++ Lisp/WebAppTrial/web-app-trial.lisp
@@ -1,16 +1,17 @@
 (in-package :web-app-trial)
 
-(defvar *acc*)
+(defparameter *acceptors* nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun stop ()
-  (hunchentoot:stop *acc*))
+  (dolist (acceptor *acceptors*) 
+    (hunchentoot:stop acceptor)))
 
 (defun run () 
   (let ((some-var))
     (format t "This will start the server.~%")
     (format t "You can access the documentation at http://localhost:4242/hunchentoot-doc.html~%")
-    (setq *acc* (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))))
+    (push (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)) *acceptors*)))
 
 ;;;==================================================
 (format t "~&Type (web-app-trial:run) to start the program")
