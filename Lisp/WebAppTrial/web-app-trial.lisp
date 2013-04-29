92c7403d9f9756338e92fb6dad5454509e30cfde
Jacek <ruby.object@googlemail.com>
2013-04-30 00:26:19 +0100

    some progress exploring documentation

--- Lisp/WebAppTrial/web-app-trial.lisp
+++ Lisp/WebAppTrial/web-app-trial.lisp
@@ -4,14 +4,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun stop ()
-  (dolist (acceptor *acceptors*) 
+  (dolist (acceptor *acceptors*)
+    (format t "going to stop: ~S~%" acceptor)
     (hunchentoot:stop acceptor)))
 
+(defun poker ()
+  (dolist (this-acceptor *acceptors*)
+    (format t "found: ~S~%" this-acceptor)
+    ))
+
 (defun run () 
-  (let ((some-var))
+  (let ((my-acceptor))
     (format t "This will start the server.~%")
     (format t "You can access the documentation at http://localhost:4242/hunchentoot-doc.html~%")
-    (push (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)) *acceptors*)))
+    (setf my-acceptor (make-instance 'hunchentoot:easy-acceptor :port 4242))
+    (push (hunchentoot:start my-acceptor) *acceptors*)
+   
+    ))
 
 ;;;==================================================
 (format t "~&Type (web-app-trial:run) to start the program")
