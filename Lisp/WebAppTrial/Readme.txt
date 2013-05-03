14129311441d6142e7b2b21c1730d30b94e1b3f7
Jacek <ruby.object@googlemail.com>
2013-05-03 08:21:35 +0100

    got the example working

--- Lisp/WebAppTrial/Readme.txt
+++ Lisp/WebAppTrial/Readme.txt
@@ -1,8 +1,7 @@
 
-RUNNING
-
 To run web app trial please type following in repl:
 
 (push #p "/home/jacek/Programming/Pyrulis/Lisp/WebAppTrial/" asdf:*central-registry*)
 (ql:quickload :web-app-trial)
-(web-app-trial:run)
+(in-package :web-app-trial)
+(run)
--- Lisp/WebAppTrial/web-app-trial.lisp
+++ Lisp/WebAppTrial/web-app-trial.lisp
@@ -1,26 +1,28 @@
 (in-package :web-app-trial)
 
-(defparameter *acceptors* nil)
+(defparameter *my-acceptors* nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun stop ()
-  (dolist (acceptor *acceptors*)
+  (dolist (acceptor *my-acceptors*)
     (format t "going to stop: ~S~%" acceptor)
     (hunchentoot:stop acceptor)))
 
 (defun poker ()
-  (dolist (this-acceptor *acceptors*)
-    (format t "found: ~S~%" this-acceptor)
-    ))
+  (dolist (this-acceptor *my-acceptors*)
+    (format t "found: ~S~%" this-acceptor)))
 
 (defun run () 
   (let ((my-acceptor))
     (format t "This will start the server.~%")
     (format t "You can access the documentation at http://localhost:4242/hunchentoot-doc.html~%")
     (setf my-acceptor (make-instance 'hunchentoot:easy-acceptor :port 4242))
-    (push (hunchentoot:start my-acceptor) *acceptors*)
-   
-    ))
+    (push (hunchentoot:start my-acceptor) *my-acceptors*)))
+
+(hunchentoot:define-easy-handler (say-hey :uri "/hi") (name)
+  (setf (hunchentoot:content-type*) "text/html")
+  (format nil "<html><head><title>hi</title></head><body></body><h1>Hi ~a</h1><p>~s</p></html>" name name))
 
 ;;;==================================================
-(format t "~&Type (web-app-trial:run) to start the program")
+(format t "~&Type (in-package :web-app-trial) and then (run) to start the program, 
+then visit: http://localhost:4242/hi?name=Jack")
