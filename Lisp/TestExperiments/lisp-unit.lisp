b4f767a155f75761cb96e72eea596aa5f98091c6
Jacek Podkanski <ruby.object@googlemail.com>
2013-06-15 23:21:00 +0100

    added simple test

---
+++ Lisp/TestExperiments/lisp-unit.lisp
@@ -0,0 +1,18 @@
+(ql:quickload :lisp-unit)
+
+(defpackage :simple-tests
+  (:use :common-lisp :lisp-unit))
+
+(in-package :simple-tests)
+
+(define-test test-addition
+  "test simple addition"
+  (assert-equal 3 (+ 1 2))
+  (assert-equal 1 (+ 0 0)))
+
+(define-test test-multiplication
+  "test simple multiplication"
+  (assert-equal 9 (* 3 3)))
+
+;; (run-tests :all)
+(run-tests '(test-addition test-multiplication))
