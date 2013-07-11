d5466e509b4a329f0a87a4e29d7ee5648d0cea1e
Jacek <ruby.object@googlemail.com>
2013-07-11 02:38:00 +0100

    refactoring

--- Lisp/SGFanalyser/sgf-analyser.lisp
+++ Lisp/SGFanalyser/sgf-analyser.lisp
@@ -31,36 +31,6 @@
              (position (char coordinates i) *sgf-letters*)))
     (cons (coord 0) (coord 1))))
 
-(defun place-stone (board colour coordinates)
-  (setf (aref board (car coordinates) (cdr coordinates)) colour))
-
-(defun add-handicaps (board)
-  (dolist (handis `(("B" ,(header-value "AB")) ("W" ,(header-value "AW"))))
-    (dolist (pos (cadr handis))
-      (format t "#### going to place ~A at ~A ~A~%" (car handis) pos (sgf-to-i pos))
-      (place-stone board (car handis) (sgf-to-i pos)))))
-
-(defun print-board (board)
-  (let ((size (car (array-dimensions board))) (stone))
-    (format t "~%    ")
-    (dotimes (c (length *board-column-letters*))
-      (format t "~a " (char *board-column-letters* c )))
-    (dotimes (r size)
-      (format T "~&~2d  " (- size r ))
-      (dotimes (c size)
-	(setf stone (aref board c r))
-	(format t "~2a"
-		(if stone
-		    stone
-		    "."))))
-    (format t "~%~%")))
-
-(defun stone-at (board coordinates)
-  (if (and (valid-coordinate-p (car coordinates))
-           (valid-coordinate-p (cdr coordinates)))
-      (aref board (car coordinates) (cdr coordinates))
-      :outside))
-
 (defun board-edge-p (coordinate)
   (or (eq coordinate 0)
       (eq coordinate (1- *board-size*))))
@@ -74,48 +44,74 @@
   ((size :reader size :initarg :size)
    (board :accessor board :initarg :board)))
 
-(defgeneric obj-add-handicaps (goban))
-(defgeneric obj-print-board (goban))
-(defgeneric obj-place-stone (goban colour coordinates))
+(defgeneric add-handicaps (goban))
+(defgeneric print-board (goban))
+(defgeneric stone-at (goban coordinates))
+(defgeneric place-stone (goban colour coordinates))
 (defgeneric neighbours (goban coordinates))
 
-(defmethod obj-add-handicaps (self)
-  (add-handicaps (slot-value self 'board)))
+(defmethod stone-at ((self goban) coordinates)
+  (if (and (valid-coordinate-p (car coordinates))
+           (valid-coordinate-p (cdr coordinates)))
+      (aref (board self) (car coordinates) (cdr coordinates))
+      :outside))
 
-(defmethod obj-print-board (self)
-  (print-board (slot-value self 'board)))
+(defmethod add-handicaps ((self goban))
+  (dolist (handis `(("B" ,(header-value "AB")) ("W" ,(header-value "AW"))))
+    (dolist (pos (cadr handis))
+      (format t "#### going to place ~A at ~A ~A~%" (car handis) pos (sgf-to-i pos))
+      (place-stone self (car handis) (sgf-to-i pos)))))
 
-(defmethod obj-place-stone ((self goban) colour coordinates)
+(defmethod print-board ((self goban))
+  (format t "in print-board")
+  (let ((size (car (array-dimensions (board self)))) (stone))
+    (format t "~%    ")
+    (dotimes (c (length *board-column-letters*))
+      (format t "~a " (char *board-column-letters* c )))
+    (dotimes (r size)
+      (format T "~&~2d  " (- size r ))
+      (dotimes (c size)
+        (setf stone (aref (board self) c r))
+        (format t "~2a"
+                (if stone
+                    stone
+                    "."))))
+    (format t "~%~%")))
+
+(defmethod place-stone ((self goban) colour coordinates)
   (setf (aref (slot-value self 'board) (car coordinates) (cdr coordinates)) colour))
 
 (defmethod neighbours ((self goban) coordinates)
   (format t "~&will try to find neighbours for ~s     edges ~s:~s   ~%"
           coordinates (board-edge-p (car coordinates)) (board-edge-p (cdr coordinates)))
-  (let ((lives) (whites) (blacks) (board (slot-value self 'board)))
+  (let ((lives) (whites) (blacks))
     (dolist (sides '((0 . -1)(1 . 0)(0 . 1)(-1 . 0)))
-      (format t "~s " (stone-at board (cons (+ (car sides) (car coordinates))
-                                            (+ (cdr sides) (cdr coordinates))))))))
+      (format t "~s " (stone-at self (cons (+ (car sides) (car coordinates))
+                                           (+ (cdr sides) (cdr coordinates))))))
+    (stone-at self coordinates)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun run ()
   (let ((board) (coordinates))
     (format T "~%~%~A <<<<<<<<<<~%" *game-record*)
     (game-stats )
     (format t "~%~d <<< board size ~%" *board-size*)
-    (setf board (make-array `(,*board-size* ,*board-size*) :initial-element nil))
-    (setf *goban* (make-instance 'goban :size *board-size* :board board))
+    (setf *goban* (make-instance 'goban 
+                                 :size *board-size* 
+                                 :board (make-array `(,*board-size* ,*board-size*) 
+                                                    :initial-element nil)))
 
-    (obj-add-handicaps *goban*)
+    (add-handicaps *goban*)
 
     (dolist (move (subseq (cdr *game-record*) 0 20))
       (format t "color ~S coordinates ~S~%" (caar move) (sgf-to-i (cdar move)))
-      (obj-place-stone *goban* (caar move) (sgf-to-i (cdar move))))
+      (place-stone *goban* (caar move) (sgf-to-i (cdar move))))
 
-    (obj-print-board *goban*)
+    (print-board *goban*)
 
     (setq coordinates (enter-coordinates))
     (format t "the coordinates are: ~A~%"  coordinates)
 
-    (format t "~A"  (stone-at board coordinates))
+    (format t "~A"  (stone-at *goban* coordinates))
     (neighbours *goban* coordinates)
     (format t ">>>>>>>> ~S" *goban*)
     ))
