ac6b1276b10d5377e9759ee5d47e925251d0c566
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-06 05:29:44 +0000

    no progress

--- Perl/tape-equilibrium.pl
+++ Perl/tape-equilibrium.pl
@@ -4,18 +4,42 @@ use strict;
 use warnings;
 
 # http://codility.com/demo/take-sample-test/tapeequilibrium
+sub sumarray{
+    my @x = @_;
+    print @x."aaa\n";
+}
 
 sub solution {
     my (@a)=@_;
     my $n = (scalar @a);
-    print $n."\n";
+    my $head;
+    my $tail;
+    my $sum1;
+    my $sum2;
+    my $res;
+    my $res2;
     for (my $p = 1; $p < $n; $p++) {
-        print $p."  vvvvvvvvvvv\n";
+        print "\n".$p."  vvvvvvvvvvv\n";
         # How do I get part of an array in Perl ?????
-        print @a[0 .. $p]."\n";
-        print @a[$p+1 .. -1]."\n";
-        print "zzzz\n";
+        $sum1 = 0;
+        map { $sum1 += $_ } @a[0 .. $p-1];
+        print $sum1."\n";
+
+        $sum2 = 0;
+        map {$sum2 += $_} @a[$p .. (scalar(@a)-1)];
+        print $sum2."\n";
+        $res = abs($sum1 - $sum2);
+        print $res;
+        if ($p == 1) {
+            $res2 = $sum1;
+        } else {
+            if ($res < $res2) {
+                $res2 = $res;
+            }
+        }
+
     }
+    $res2;
 }
 
 print "\n";
@@ -23,4 +47,10 @@ print "\n";
 print solution (3, 1, 2, 4, 3);
 
 
+print "\n";
+
+my @rrr = (1,2,3,4,5,6,7);
+print @rrr;
+print "\n";
+print @rrr[2 .. (scalar(@rrr) -2)];
 print "\n";
