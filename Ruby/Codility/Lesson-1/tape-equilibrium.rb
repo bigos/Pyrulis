f018e75a631a4f9c46d91210c16eb291b347b5b7
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-09 12:20:36 +0000

    tape-equilibrium.rb passed all tests

--- Ruby/Codility/Lesson-1/tape-equilibrium.rb
+++ Ruby/Codility/Lesson-1/tape-equilibrium.rb
@@ -1,27 +1,26 @@
 def solution(a)
   n = a.size
-  r = a.reverse
-  p a
+  min = 2000
+  b = a.reverse
+  #p a
   for x in 1 .. n-1
     a[x] = a[x-1] + a[x]
-    r[x] = r[x-1] + r[x]
-
+    b[x] = b[x-1] + b[x]
   end
-    puts "#{x} : #{a.inspect}  #{r.inspect}"
-  r.reverse!
+  #puts "#{x} : #{a.inspect}  #{b.inspect}"
+  b.reverse!
   #p a
-  #p r
-  r1 = 200
+  #p b
   for x in 0 .. a.size-2
     s=a[x]
-    e=r[x+1]
+    e=b[x+1]
     r2 = (s - e).abs
     #puts "#{s} #{e}  #{r2}"
-    if r2 < r1
-      r1 = r2
+    if r2 < min
+      min = r2
     end
   end
-  r1
+  min
 end
 
 p solution [3, 1, 2, 4, 3]
