3360448a99b5118ae80ceb3463934b23ec5ce985
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-10 14:02:44 +0000

    still too slow

--- Ruby/Codility/Lesson-2/max-counters.rb
+++ Ruby/Codility/Lesson-2/max-counters.rb
@@ -1,24 +1,23 @@
 class Counters
   def initialize(n)
     @counters = Array.new(n,0)
+    @max = 0
   end
   def increase(x)
     @counters[x-1] += 1
-  end
-  def max_counter
-    max = @counters.max
-    for x in 0 .. (@counters. size) -1
-      @counters[x]=max
-    end
-    @counters
   end
   def solution(n,a)
     m = a.size
     for k in 0 .. m-1
       if 1 <= a[k] and a[k] <= n
-        increase(a[k])
+        premax = increase(a[k])
+        @max = premax if premax > @max
       elsif a[k] == n + 1
-        max_counter
+        # max_counter
+        for x in 0 .. (@counters.size) -1
+          @counters[x]=@max
+        end
+        @counters
       else
         raise 'error'
       end
@@ -40,5 +39,5 @@ puts Benchmark.measure {
 }
 
 puts Benchmark.measure {
-  p solution(500,([501]*555))
+  p solution(500,[1,2,3,4,5,6,2,2,2]+([1,501]*555))
 }
