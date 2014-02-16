dc39cee7c63fbdfa703ead7d966d43f0ee9145b2
J. Podkanski <ruby.object@googlemail.com>
2014-02-16 19:00:07 +0000

    added comment about correct order of package definitions

--- Lisp/impex/package.lisp
+++ Lisp/impex/package.lisp
@@ -7,5 +7,7 @@
 (defpackage #:baz
   (:use #:cl) (:export :baz1))
 
+;; this package should go last in order
+;; to correctly use above packages
 (defpackage #:impex
   (:use #:cl #:foo #:bar #:baz))
