58c5ea6b7e005c5d08b4176705cd64cf8f073437
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-03 23:41:45 +0000

    sum-of-2

--- Scheme/beginning.scm
+++ Scheme/beginning.scm
@@ -10,12 +10,12 @@
   (cond ((< x 0) (- x))
         (else x)))
 
-(define (larger a b)
-  (cond ((> a b) a)
-        (else b)))
-
 ;Exercise 1.2.
 (/(+ 5 4(- 2(- 3 (+ 6 (/ 4 5)))))
   (* 3(- 6 2)(- 2 7)))
 
-;todo excercise 1.3
+;excercise 1.3
+(define (sum-of-2-largest-of-3 a b c)
+  (+(max a b)
+    (max b c))) 
+  
