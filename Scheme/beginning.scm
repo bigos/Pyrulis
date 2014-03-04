407c52da7d46ef9f642e56c213826af5df210723
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-04 00:05:58 +0000

    added square root calculation

--- Scheme/beginning.scm
+++ Scheme/beginning.scm
@@ -14,8 +14,26 @@
 (/(+ 5 4(- 2(- 3 (+ 6 (/ 4 5)))))
   (* 3(- 6 2)(- 2 7)))
 
-;excercise 1.3
 (define (sum-of-2-largest-of-3 a b c)
   (+(max a b)
     (max b c))) 
   
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess(/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.00001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; todo excercise 1.8
