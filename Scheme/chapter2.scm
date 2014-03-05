4ef811942d0d2c55e218cc818146e722dbe6e4d4
J. Podkanski <ruby.object@googlemail.com>
2014-03-05 02:17:24 +0000

    new excercise

--- Scheme/chapter2.scm
+++ Scheme/chapter2.scm
@@ -13,8 +13,8 @@
                  (fib (- n 2))))))
 
 ;iterative
-(define (fib n)
-  (fib-iter 1 0 n))
+;; (define (fib n)
+;;   (fib-iter 1 0 n))
 
 (define (fib-iter a b count)
   (if (= count 0)
@@ -39,3 +39,24 @@
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+;; Exercise 1.11.
+;; A function f is defined by the rule that
+;; f(n) = n if n<3
+;; and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3.
+;; Write a procedure that computes f by means of a recursive process.
+;; Write a procedure that computes f by means of an iterative process.
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (* 1 (f (- n 1)))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+; todo: finish it tomorrow
+(define (ff n)
+  (if (< n 3)
+      n
+      (fiter n))
+  (define (fiter n)
+    ))
