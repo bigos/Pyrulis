eebd3cf4bd7efc5fdbde6a328f800b9780446566
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-10 13:54:18 +0000

    added iterative version of the excercise

--- Scheme/chapter2.scm
+++ Scheme/chapter2.scm
@@ -119,3 +119,11 @@
   (cond ((= n 0) 1)
         ((even? n) (square (fast-expt b (/ n 2))))
         (else (* b (fast-expt b (- n 1))))))
+; iterative version
+(define (fast-expt-i b n)
+  (fast-expt-iter b n 1))
+
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* b a)))))
