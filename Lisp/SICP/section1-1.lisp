90303f364c16e9ca80e55027fc351fa76f5e08e6
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-11 00:00:42 +0000

    added two excercises

--- Lisp/SICP/section1-1.lisp
+++ Lisp/SICP/section1-1.lisp
@@ -9,3 +9,16 @@
   (let ((tl (two-larger a b c)))
     (+ (expt (car tl) 2)
        (expt (cdr tl) 2))))
+
+;; Exercise 1.4
+(defun a-plus-abs-b (a b)
+    (funcall (if (> b 0) #'+ #'-) a b))
+
+;;Exercise 1.5
+
+(defun p () (funcall #'p))
+
+(defun test (x y)
+    (if (= x 0)
+        0
+        (funcall y)))
