e9283c4c5354818f9a28769e6ada5ec0779014f3
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-12 00:23:07 +0000

    added coin counting function

--- Lisp/SICP/section1-1.lisp
+++ Lisp/SICP/section1-1.lisp
@@ -99,3 +99,34 @@
      and b = 1 then (+ a b)
      until (= count n)
      finally (return a)))
+
+;; Example: Counting change
+(defun count-change (amount)
+  (cc amount 5))
+
+(defun cc (amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (T (+ (cc amount
+                  (- kinds-of-coins 1))
+              (cc (- amount
+                     (first-denomination kinds-of-coins))
+                  kinds-of-coins)))))
+
+(defun first-denomination (kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; Exercise 1.11
+;;TODO
+
+;; Exercise 1.12
+;;TODO
+
+;; Exercise 1.13
+;;TODO
+
+;; 1.2.3  Orders of Growth
