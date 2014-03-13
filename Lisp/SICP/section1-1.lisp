6a44df8f8d02f1f7a424a4e6435585b81f63d25f
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-13 01:03:41 +0000

    finished excercise 1.16

--- Lisp/SICP/section1-1.lisp
+++ Lisp/SICP/section1-1.lisp
@@ -159,9 +159,15 @@
 ;; 1.2.3  Orders of Growth
 
 ;; 1.2.4  Exponentiation
-;; Exercise 1.16.  Design a procedure that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt. (Hint: Using the observation that (bn/2)2 = (b2)n/2, keep, along with the exponent n and the base b, an additional state variable a, and define the state transformation in such a way that the product a bn is unchanged from state to state. At the beginning of the process a is taken to be 1, and the answer is given by the value of a at the end of the process. In general, the technique of defining an invariant quantity that remains unchanged from state to state is a powerful way to think about the design of iterative algorithms.)
+
+;; Exercise 1.16
 
 (defun fast-expt (b n)
   (cond ((= n 0)1)
         ((evenp n) (expt (fast-expt b (/ n 2) ) 2))
         (T (* b (fast-expt b (1- n))))))
+
+(defun fast-expt-iter (b n &optional (a 1))
+  (cond ((= n 0) a)
+        ((evenp n) (fast-expt-iter (expt b 2) (/ n 2) a))
+        (T (fast-expt-iter b (- n 1) (* a b)))))
