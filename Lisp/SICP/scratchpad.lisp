8990ea1055e6a21f5da0c854a1838c8c36198be0
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-21 20:07:21 +0000

    experimenting

--- Lisp/SICP/scratchpad.lisp
+++ Lisp/SICP/scratchpad.lisp
@@ -1,11 +1,26 @@
 ;; my experiment with calculating square root
 
-(defun good-enough? (result expected tolerance)
-  (<= (- expected result) tolerance))
+(defun good-enough? (operator result expected tolerance)
+  (format t "~&trying ~f ~f ~f ~f~%" operator result expected tolerance)
+  (and
+   (funcall operator expected result)
+   (< (abs (- expected result)) tolerance)))
 
-(defun sqr (n &optional (guess 0) (tolerance 1))
-  (format t "~S ~S     " guess tolerance)
-  (cond ((zerop n) 0)
-        ((eq 1 n) 1)
-        ((good-enough? (* guess guess) n 0.01) guess)
-        (T (sqr n (+ tolerance guess) tolerance))))
+(defun average (min max) (/ (+ min max) 2))
+(defun square (n) (* n n))
+(defun halve (n) (/ n 2))
+
+(defparameter *counter* 10)
+
+(defun sqr (n &optional (guess 0) (tolerance n))
+  (if (> *counter* 20)
+      (error "finished")
+      (incf *counter*))
+  (format t "   ~S ~S " guess tolerance)
+  (cond ((good-enough? '< (square guess) n (square tolerance))
+         (format t "A")
+         (sqr n guess (halve tolerance)))
+        ((good-enough? '>= (square guess) n (square tolerance))
+         (format t "B")
+         (sqr n (halve tolerance) (halve tolerance)))
+        (T (error "strange error"))))
