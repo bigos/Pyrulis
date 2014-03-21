0c903d6855ce82f4bdcbd569ccd7731064d70642
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-21 00:20:27 +0000

    experiment

--- Lisp/SICP/scratchpad.lisp
+++ Lisp/SICP/scratchpad.lisp
@@ -3,9 +3,9 @@
 (defun good-enough? (result expected tolerance)
   (<= (- expected result) tolerance))
 
-(defun sqr (n &optional (guess 0))
-  (format t "~S    " guess)
+(defun sqr (n &optional (guess 0) (tolerance 1))
+  (format t "~S ~S     " guess tolerance)
   (cond ((zerop n) 0)
         ((eq 1 n) 1)
         ((good-enough? (* guess guess) n 0.01) guess)
-        (T (sqr n (+ 0.01 guess)))))
+        (T (sqr n (+ tolerance guess) tolerance))))
