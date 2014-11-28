9e979e3ee8573d5b75491216a00be7c4c5cd1d44
J. Podkanski <ruby.object@googlemail.com>
2014-11-28 23:26:22 +0000

    improved readme file

--- Lisp/WebAppTrial/Readme.txt
+++ Lisp/WebAppTrial/Readme.txt
@@ -1,7 +1,9 @@
-
 To run web app trial please type following in repl:
 
 (push #p "/home/jacek/Programming/Pyrulis/Lisp/WebAppTrial/" asdf:*central-registry*)
 (ql:quickload :web-app-trial)
 (in-package :web-app-trial)
 (run)
+
+To refresh the web content after editing the source run:
+(ql:quickload :web-app-trial)
--- Lisp/WebAppTrial/views.lisp
+++ Lisp/WebAppTrial/views.lisp
@@ -12,24 +12,24 @@
       (:a :href "/faa" "More")))))
 
 
-(defun faa1-view () 
+(defun faa1-view ()
   (who:with-html-output-to-string (out)
     (:html
      (:head
       (:title "yet another Lisp web app")
       (:link :href "/style.css" :media "all" :rel "stylesheet" :type "text/css"))
-     (:body  
+     (:body
       (:div
        (:a :href "/" "see the index")
-       (:span :style "margin:0 2em	;" "|")
+       (:span :style "margin:0 2em;" "|")
        (:a :href "/about_me?name=Jacek&language=Lisp" "info about me")
        (:span :style "margin:0 2em;" "|")
        (:a :href "/hunchentoot-doc.html" "Documentation"))
-      (:hr)    
+      (:hr)
       (:h1 :id "heading" "Hello Lispers")
       (:p :class "message"  "Hi everybody, we have lift off.")
       (:p "acceptor object" (escaped-string  hunchentoot:*acceptor*))
-      (:footer "&copy; 2013 Jacek Podkanski")))))
+      (:footer "&copy; 2014 Jacek Podkanski")))))
 
 (defun foo1-view ()
   (who:with-html-output-to-string (out)
@@ -37,14 +37,14 @@
      (:head
       (:title "This is foo")
       (:link :href "/style.css" :media "all" :rel "stylesheet" :type "text/css")
-      (:script :src "/javascript.js" )) 
+      (:script :src "/javascript.js" ))
      (:body
       (:h1 "Foo")
       (:a :href "/faa" "faa")
       (:p "foo foo foo"
-	  (escaped-string " <tag>text</tag> y ")
-	  (escaped-string hunchentoot:*request*)
-	  (who:fmt "~s" (hunchentoot:get-parameters*)))
+          (escaped-string " <tag>text</tag> y ")
+          (escaped-string hunchentoot:*request*)
+          (who:fmt "~s" (hunchentoot:get-parameters*)))
       (:p
        (who:fmt "~a"  (pp-object hunchentoot:*request*)))
       (hunchentoot:log-message* :info "abcdef ~D~%~%~A~%" 123 (inspect-object hunchentoot:*reply*))
