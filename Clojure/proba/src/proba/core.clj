2b04cc63e8a3aa533b8fa65704349c93661ae5c0
J. Podkanski <ruby.object@googlemail.com>
2014-12-15 21:32:02 +0000

    old Clojure files

---
+++ Clojure/proba/src/proba/core.clj
@@ -0,0 +1,4 @@
+(ns proba.core)
+
+(defn -main []
+"Hello World!")
---
+++ Clojure/proba/test/proba/test/core.clj
@@ -0,0 +1,6 @@
+(ns proba.test.core
+  (:use [proba.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
