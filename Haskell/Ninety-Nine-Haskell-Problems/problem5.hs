ec4487e1e90babe78a45843fdcc19866cb17fe45
Jacek Podkanski <ruby.object@googlemail.com>
2014-12-20 11:58:17 +0000

    function rename

--- Haskell/Ninety-Nine-Haskell-Problems/problem5.hs
+++ Haskell/Ninety-Nine-Haskell-Problems/problem5.hs
@@ -1,6 +1,5 @@
  -- 5 Problem 5
 -- (*) Reverse a list.
-
-reverse :: [a] -> [a]
-reverse [] = []
-reverse (x:xs) = reverse xs ++ [x]
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = reverse xs ++ [x]
