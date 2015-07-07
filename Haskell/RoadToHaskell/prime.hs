873c183c0a1bc27d325c2554184ae71cb6635246
Jacek Podkanski <ruby.object@googlemail.com>
2015-07-07 12:57:14 +0100

    examples up to page 19

--- Haskell/RoadToHaskell/prime.hs
+++ Haskell/RoadToHaskell/prime.hs
@@ -1,1 +1,14 @@
 divides d n = rem n d == 0
+
+ld n = ldf 2 n
+
+ldf k n | divides k n = k
+        | k^2 > n = n
+        | otherwise = ldf (k+1) n
+
+prime0 n | n < 1 = error "not a positive integer"
+         | n == 1 = False
+         | otherwise = ld n == n
+
+
+{- page 19 -}
