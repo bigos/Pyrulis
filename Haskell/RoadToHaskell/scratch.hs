27bc0b31b8edeb3628265ade97c66c029488834a
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-07-14 15:13:31 +0100

    stuck

--- Haskell/RoadToHaskell/scratch.hs
+++ Haskell/RoadToHaskell/scratch.hs
@@ -1,3 +1,8 @@
+removeFst' :: Int -> [Int] -> [Int] -> [Int]
+removeFst' i [] result = []
+removeFst' i (x:xs) result | i == x = result
+removeFst' i lints result = removeFst' i z
+
 removeFst :: Int -> [Int] -> [Int]
 removeFst i lints | lints == [] = lints
-                  | otherwise = removeFst i lints
+                  | otherwise = removeFst' i lints []
