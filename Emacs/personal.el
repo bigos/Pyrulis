af840277034f362788b700f0f252d110aca14092
Jacek Podkanski <ruby.object@googlemail.com>
2015-07-24 22:32:49 +0100

    updated colours

--- Emacs/personal.el
+++ Emacs/personal.el
@@ -79,12 +79,12 @@
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
    '(rainbow-delimiters-depth-1-face ((t (:foreground "grey"))))
-   '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
-   '(rainbow-delimiters-depth-3-face ((t (:foreground "green"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "RoyalBlue"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "lime green"))))
    '(rainbow-delimiters-depth-4-face ((t (:foreground "yellow"))))
    '(rainbow-delimiters-depth-5-face ((t (:foreground "red"))))
-   '(rainbow-delimiters-depth-6-face ((t (:foreground "blue"))))
-   '(rainbow-delimiters-depth-7-face ((t (:foreground "green"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "RoyalBlue"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "lime green"))))
    '(rainbow-delimiters-depth-8-face ((t (:foreground "yellow"))))
    '(rainbow-delimiters-depth-9-face ((t (:foreground "red"))))
    '(rainbow-delimiters-unmatched-face ((t (:foreground "white" :background "red"))))
