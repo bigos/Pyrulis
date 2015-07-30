4886a0a62b83069a176e73f1ea92fb0c3ef55871
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-07-30 10:34:44 +0100

    color work

--- Emacs/personal.el
+++ Emacs/personal.el
@@ -80,32 +80,29 @@
 
 (defun bracket-colors ()
   (let (hexcolors)
-    (dolist (n '(1.0 0.6 .3 .15) hexcolors )
-      (push (hsl-to-hex n 1.0 0.55) hexcolors))))
+    (reverse (dolist (n '(0.72 .4 .13 .90) hexcolors )
+                  (push (hsl-to-hex (+ n 0.0) 1.0 0.55) hexcolors)))))
 
 
 (defun colorise-brackets ()
   (require 'rainbow-delimiters)
   (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
    ;; emacs colours
    ;; http://raebear.net/comp/emacscolors.html
    ;; or use (list-colors-display)
    `(rainbow-delimiters-depth-1-face ((t (:foreground "grey"))))
-   `(rainbow-delimiters-depth-2-face ((t (:foreground ,(elt(bracket-colors) 3)))))
-   `(rainbow-delimiters-depth-3-face ((t (:foreground ,(elt(bracket-colors) 2)))))
-   `(rainbow-delimiters-depth-4-face ((t (:foreground ,(elt(bracket-colors) 1)))))
-   `(rainbow-delimiters-depth-5-face ((t (:foreground ,(elt(bracket-colors) 0)))))
-   `(rainbow-delimiters-depth-6-face ((t (:foreground ,(elt(bracket-colors) 3)))))
-   `(rainbow-delimiters-depth-7-face ((t (:foreground ,(elt(bracket-colors) 2)))))
-   `(rainbow-delimiters-depth-8-face ((t (:foreground ,(elt(bracket-colors) 1)))))
-   `(rainbow-delimiters-depth-9-face ((t (:foreground ,(elt(bracket-colors) 0)))))
+   `(rainbow-delimiters-depth-2-face ((t (:foreground ,(elt(bracket-colors) 0)))))
+   `(rainbow-delimiters-depth-3-face ((t (:foreground ,(elt(bracket-colors) 1)))))
+   `(rainbow-delimiters-depth-4-face ((t (:foreground ,(elt(bracket-colors) 2)))))
+   `(rainbow-delimiters-depth-5-face ((t (:foreground ,(elt(bracket-colors) 3)))))
+   `(rainbow-delimiters-depth-6-face ((t (:foreground ,(elt(bracket-colors) 0)))))
+   `(rainbow-delimiters-depth-7-face ((t (:foreground ,(elt(bracket-colors) 1)))))
+   `(rainbow-delimiters-depth-8-face ((t (:foreground ,(elt(bracket-colors) 2)))))
+   `(rainbow-delimiters-depth-9-face ((t (:foreground ,(elt(bracket-colors) 3)))))
    `(rainbow-delimiters-unmatched-face ((t (:foreground "white" :background "red"))))
    `(highlight ((t (:foreground "#ff0000" :background "grey"))))
    ))
+
 (colorise-brackets)
 
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
