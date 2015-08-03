d00ab49791dcc64294100a2e482c55954ef0eca2
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-03 08:48:12 +0100

    another attempt

--- Haskell/RoadToHaskell/scratch.hs
+++ Haskell/RoadToHaskell/scratch.hs
@@ -1,8 +1,2 @@
-removeFst' :: Int -> [Int] -> [Int] -> [Int]
-removeFst' i [] result = []
-removeFst' i (x:xs) result | i == x = result
-removeFst' i lints result = removeFst' i z
-
 removeFst :: Int -> [Int] -> [Int]
-removeFst i lints | lints == [] = lints
-                  | otherwise = removeFst' i lints []
+removeFst i [] = []
