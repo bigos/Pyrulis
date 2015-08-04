05ec1efb419110868821144c8ed9cf04f302d167
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-08-04 09:13:37 +0100

    zzz

--- Haskell/RoadToHaskell/scratch.hs
+++ Haskell/RoadToHaskell/scratch.hs
@@ -1,2 +1,7 @@
+remit i a n | head(snd(s)) == n = (fst s) + (tail(snd(s)))
+            | otherwise = remit i a (n + 1)
+  where s =  splitAt n a
+
 removeFst :: Int -> [Int] -> [Int]
 removeFst i [] = []
+removeFst i a = remit i a 0
