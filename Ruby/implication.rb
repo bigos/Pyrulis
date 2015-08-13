b6140ef19561fdd099cda56a4da750824752c962
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-13 12:45:28 +0100

    ruby implication cleanup

--- Ruby/implication.rb
+++ Ruby/implication.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-puts 'testing implications'
+puts 'testing implication if a is true then b must be true'
 # run like this from Emacs eshell
 # ~/.rvm/rubies/ruby-2.2.1/bin/ruby ./implication.rb
 
@@ -11,9 +11,20 @@ def b(n) 3 < n end
 [2,4,6].each do |n|
   ares = a n
   bres = b n
-  print n, ' ',bres, ' ', ares, ' - ', (!bres || ares)
-  print "\t -  "
-  print n, ' ',ares, ' ', bres, ' - ', (!ares || bres)
+  result = (!bres || ares)
+  comment = "\t "
+  if result
+    if ares
+      comment << 'true - the statement is telling the truth'
+    else
+      comment << 'true - innocent unless proven guilty'
+    end
+  else
+    comment << 'false - LIAR!!!'
+  end
+
+  print n, ' ',bres, ' ', ares, ' - ', result, comment
+
   puts
 end
 
@@ -21,5 +32,6 @@ puts '---------------'
 
 v = [[true, true], [true, false], [false, true], [false, false]]
 v.each do |n|
-  p !n[0] || n[1]
+  a, b = n
+  p !a || b
 end
