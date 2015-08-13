54819236e21bf9bbb43bee89b4cb8b5fee9c42bb
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-13 22:24:40 +0100

    different path

--- Haskell/RoadToHaskell/chapter2.hs
+++ Haskell/RoadToHaskell/chapter2.hs
@@ -26,10 +26,15 @@ p ==> q = (not p) || q
 -- if I win the elections the taxes will not go down
 
 verdict :: Bool -> Bool -> (Bool, String)
-verdict p q | not result  = (False, "LIAR")
-            | result && p = (True,  "telling the truth")
-            | result      = (True,  "innocent unless proven guilty")
-  where result = p ==> q
+verdict p q = verdict2 p (p ==> q)
 -- map (\x -> verdict (fst x == 1) (snd x == 1)) [(1,1),(1,0),(0,1),(0,0)]
+
+verdict2 :: Bool -> Bool -> (Bool, String)
+verdict2 True True  = (True, "telling the truth")
+verdict2 True False = (False, "LIAR")
+verdict2 False _    = (True, "innocent unless proven guilty")
+-- will get warning - verdict2 _ True     = (True, "innocent unless proven guilty")
+-- map (\x -> verdict2 (fst x) ((fst x) ==> (snd x))) [(True,True),(True,False),(False,True),(False,False)]
+
 
 -- Equivalence page 35/46
