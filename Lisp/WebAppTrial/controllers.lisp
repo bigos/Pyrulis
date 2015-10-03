184820a98c251591be3ebdc29b4c34c451da28c6
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-03 10:59:55 +0100

    more tweaking

--- Lisp/WebAppTrial/controllers.lisp
+++ Lisp/WebAppTrial/controllers.lisp
@@ -1,25 +1,10 @@
 (in-package :web-app-trial)
 
 (defun home-page ()
-  (home-page-view))
+  (default-layout  (home-page-view)))
 
 (defun faa1 ()
-  (faa1-view))
+  (default-layout  (faa1-view)))
 
 (defun foo1 ()
-  ;;(foo1-view)
-  (default-layout (foo1-view))
-  )
-
-(defun default-layout (content)
-  (who:with-html-output-to-string (out)
-    (:html
-     (:head
-      (:title "layout")
-      (:link :href "/style.css" :media "all" :rel "stylesheet" :type "text/css")
-      (:script :src "jquery-2.1.1.min.js")
-      (:script :src "/javascript.js" ))
-     (:body
-      (:h1 "In Layout")
-      (who:fmt "~A" content)
-      (:footer "footer")))))
+  (default-layout (foo1-view)))
