75315d8b365fd4341238c99f240c254ae710b024
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-03 10:09:20 +0100

    gout basic layout working

--- Lisp/WebAppTrial/controllers.lisp
+++ Lisp/WebAppTrial/controllers.lisp
@@ -7,4 +7,19 @@
   (faa1-view))
 
 (defun foo1 ()
-  (foo1-view))
+  ;;(foo1-view)
+  (default-layout (foo1-view))
+  )
+
+(defun default-layout (content)
+  (who:with-html-output-to-string (out)
+    (:html
+     (:head
+      (:title "layout")
+      (:link :href "/style.css" :media "all" :rel "stylesheet" :type "text/css")
+      (:script :src "jquery-2.1.1.min.js")
+      (:script :src "/javascript.js" ))
+     (:body
+      (:h1 "In Layout")
+      (who:fmt "~A" content)
+      (:footer "footer")))))
