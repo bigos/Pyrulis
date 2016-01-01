13ca4f49d43188dd2403abc9400c77066390d3e4
Jacek Podkanski <ruby.object@googlemail.com>
2016-01-01 14:36:40 +0000

    got to page 17 troubleshooting

---
+++ Haskell/HaskellProgrammingFromFirstPrinciples/learn.hs
@@ -0,0 +1,9 @@
+-- learn.hs - p 17
+module Learn where
+-- First, we declare the name of our module so
+-- it can be imported by name in a project.
+-- We won't be doing a project of this size
+-- for a while yet.
+x = 10 * 5 + y
+myResult = x * 5
+y = 10
