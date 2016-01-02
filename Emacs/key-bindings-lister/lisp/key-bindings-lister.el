4ac7cf5f1bb26cc3bc6133a1bf7e802742554bde
Jacek Podkanski <ruby.object@googlemail.com>
2016-01-02 01:33:57 +0000

    got inactive keybindings as well

--- Emacs/key-bindings-lister/lisp/key-bindings-lister.el
+++ Emacs/key-bindings-lister/lisp/key-bindings-lister.el
@@ -5,6 +5,36 @@
 
 ;;; Code:
 
+;;; notes
+
+;; I can get key bindings for sinple shortcut like C-s this way:
+
+;; find the code for C-s
+
+;; ELISP> ?\C-s
+;; 19 (#o23, #x13, ?\C-s)
+
+;; which is 19
+
+;; ELISP> (key-binding [19])
+;; isearch-forward
+
+;; now C-x s
+
+;; ELISP> 24
+;; 24 (#o30, #x18, ?\C-x)
+
+;; ELISP> ?s
+;; 115 (#o163, #x73, ?s)
+
+;; ELISP> (key-binding [24 115])
+;; save-some-buffers
+
+;; now M-g
+;; ?\M-g
+
+;; BOOOO got inactive key bibdings as well
+
 (defun print-current-buffer-info ()
   (interactive)
   (let ((the-buffer (current-buffer))
@@ -14,7 +44,9 @@
     (setq my-bindings (get-content-of-buffer "*Help*"))
     (set-buffer "*Messages*")
     (print "+++++++++++++++++++++++++++++")
-    (princ my-bindings)
+    (dolist (x (split-string my-bindings "\n"))
+      (print "=====================")
+      (print x))
     (set-buffer the-buffer)
     ))
 
