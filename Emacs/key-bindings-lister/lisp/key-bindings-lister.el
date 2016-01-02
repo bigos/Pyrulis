a065f9438d3e02501a65d6d5e3fb7850129a5138
Jacek Podkanski <ruby.object@googlemail.com>
2016-01-02 00:14:35 +0000

    some progress

--- Emacs/key-bindings-lister/lisp/key-bindings-lister.el
+++ Emacs/key-bindings-lister/lisp/key-bindings-lister.el
@@ -7,8 +7,26 @@
 
 (defun print-current-buffer-info ()
   (interactive)
-  (let ((the-buffer (current-buffer)))
-    (print the-buffer)))
+  (let ((the-buffer (current-buffer))
+        (my-bindings ""))
+    (print the-buffer)
+    (describe-bindings nil the-buffer)
+    (setq my-bindings (get-content-of-buffer "*Help*"))
+    (set-buffer "*Messages*")
+    (print "+++++++++++++++++++++++++++++")
+    (princ my-bindings)
+    (set-buffer the-buffer)
+    ))
+
+(defun get-content-of-buffer (buf)
+  (let ((current (current-buffer))
+        (content))
+    (if buf
+        (progn
+          (set-buffer buf)
+          (setq content (buffer-string))
+          (set-buffer current)
+          content))))
 
 (provide 'key-bindings-lister)
 
