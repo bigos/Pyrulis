62b690d475c60eac8018fb54d331cda5b49a7f02
Jacek Podkanski <ruby.object@googlemail.com>
2016-02-20 23:18:34 +0000

    now i get compilation warning

--- Lisp/Typeexperiments/mod-safety.lisp
+++ Lisp/Typeexperiments/mod-safety.lisp
@@ -1,4 +1,17 @@
-(defun my-mod (a b)
+;; compilation stage type warnings
+;; $ sbcl --eval '(compile-file "./mod-safety.lisp")'
+
+(defmacro defn (name types args &rest rest)
+  "Type safe defun"
+  (let ((types (remove-if
+                (lambda (x) (or (equal '-> x) (equal '→ x))) types)))
+    `(progn (defun ,name ,args
+              ,@(loop for arg in args for type in types
+                   collect `(check-type ,arg ,type))
+              ,@rest)
+            (declaim (ftype (function ,(butlast types) ,@(last types)) ,name)))))
+
+(defn my-mod (integer -> integer -> integer) (a b)
   (mod a b))
 
 (defun call-my-mod ()
