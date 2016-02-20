cdcd58f9c6236148e9bde47ad33ab36e7c7d7cfd
Jacek Podkanski <ruby.object@googlemail.com>
2016-02-20 23:40:36 +0000

    compile error upon incorrect result type

--- Lisp/Typeexperiments/mod-safety.lisp
+++ Lisp/Typeexperiments/mod-safety.lisp
@@ -1,20 +1,24 @@
 ;; compilation stage type warnings
 ;; $ sbcl --eval '(compile-file "./mod-safety.lisp")'
 
-(defmacro defn (name types args &rest rest)
-  "Type safe defun"
-  (let ((types (remove-if
-                (lambda (x) (or (equal '-> x) (equal '→ x))) types)))
-    `(progn (defun ,name ,args
-              ,@(loop for arg in args for type in types
-                   collect `(check-type ,arg ,type))
-              ,@rest)
-            (declaim (ftype (function ,(butlast types) ,@(last types)) ,name)))))
+;; (defmacro defn (name types args &rest rest)
+;;   "Type safe defun"
+;;   (let ((types (remove-if
+;;                 (lambda (x) (or (equal '-> x) (equal '→ x))) types)))
+;;     `(progn (defun ,name ,args
+;;               ,@(loop for arg in args for type in types
+;;                    collect `(check-type ,arg ,type))
+;;               ,@rest)
+;;             (declaim (ftype (function ,(butlast types) ,@(last types)) ,name)))))
 
-(defn my-mod (integer -> integer -> symbol) (a b)  ;changing result to symbol doesn't warn you
+(declaim (ftype (function (integer integer) (symbol))
+                my-mod))
+
+(defun my-mod  (a b)  ;declaring incorrect results gives compile error
   (mod a b))
 
 (defun call-my-mod ()
-  (my-mod 7 3))                         ;passing correct results
+  (my-mod 7 3))                       
 
-(call-my-mod)
+(princ (call-my-mod))
+(terpri)
