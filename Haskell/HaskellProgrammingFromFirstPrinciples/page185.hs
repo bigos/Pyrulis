ec3cf515413898cdd0755820484b05f95fd16d97
Jacek Podkanski <ruby.object@googlemail.com>
2016-03-13 19:44:03 +0000

    exercise progress

---
+++ Haskell/HaskellProgrammingFromFirstPrinciples/page185.hs
@@ -0,0 +1,30 @@
+-- page 185
+-- Does it compile?
+
+-- bigNum = (^) 5 10
+-- wahoo = bigNum ^ 10
+
+-- x = print
+-- y = print "wohoo!"
+-- z = x "hello world"
+
+-- a = (+)
+-- b = 5
+-- c = a b 10
+-- d = a c 200
+
+-- Type variable or specific type constructor
+-- page 186
+
+-- functionH :: [a] -> a
+-- functionH (x:_) = x
+
+-- functionC :: Ord a => a -> a -> Bool
+-- functionC x y = if (x > y) then True else False
+
+-- functionS :: (a,b) -> b
+-- functionS (x, y) = y
+
+
+-- page 187
+-- Given a type, write a function
