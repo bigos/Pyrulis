27792fd2b0077fb670908a10db3e8278206b50be
Jacek Podkanski <ruby.object@googlemail.com>
2016-03-28 15:40:10 +0100

    stuck

---
+++ Haskell/HaskellProgrammingFromFirstPrinciples/pagesofchapter6.hs
@@ -0,0 +1,24 @@
+-- chapter 6 scribbling
+
+
+data TisAnInteger =
+TisAn Integer
+instance Eq TisAnInteger where
+  TisAnInteger' == TisAnInteger' = True
+    -- or
+    -- (==) TisAnInteger' TisAnInteger' = True
+data TwoIntegers =
+Two Integer Integer
+data StringOrInt =
+TisAnInt Int
+| TisAString String
+data Pair a =
+Pair a a
+data Tuple a b =
+Tuple a b
+data Which a =
+ThisOne a
+| ThatOne a
+data EitherOr a b =
+Hello a
+| Goodbye b
