d78e2dfef958158fce2fcfb00c0eaf3e51c1eb0e
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-28 14:10:30 +0100

    beginning of chapter 2

---
+++ Lisp/PAIP/chapter2.lisp
@@ -0,0 +1,5 @@
+(defun sentence ()    (append (noun-phrase) (verb-phrase)))
+(defun noun-phrase () (append (article) (noun)))
+(defun verb-phrase () (append (verb) (noun-phrase)))
+(defun article ()     (one-of '(the a)))
+(defun noun ()        (one-of '(man woman table)))
