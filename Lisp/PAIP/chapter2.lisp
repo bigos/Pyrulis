94899506adf93377cff435f4fca555d0575aeba9
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-28 21:05:27 +0100

    progress

--- Lisp/PAIP/chapter2.lisp
+++ Lisp/PAIP/chapter2.lisp
@@ -3,3 +3,12 @@
 (defun verb-phrase () (append (verb) (noun-phrase)))
 (defun article ()     (one-of '(the a)))
 (defun noun ()        (one-of '(man woman table)))
+(defun verb ()        (one-of '(hit took saw liked)))
+
+(defun one-of (set)
+  "Pick one element of set and make a list of it"
+  (list (random-elt set)))
+
+(defun random-elt (choices)
+  "Choose an element from a list at random"
+  (elt choices (random (length choices))))
