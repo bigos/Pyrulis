7d93dd2790f550626d0fb97f48d742b47c0d2dac
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-30 13:23:04 +0100

    baby steps

--- Lisp/PAIP/chapter2.lisp
+++ Lisp/PAIP/chapter2.lisp
@@ -27,3 +27,40 @@
 (defun pp () (append (prep) (noun-phrase)))
 (defun adj () (one-of '(big little blue green adiabatic)))
 (defun prep () (one-of '(to in by with on)))
+
+(defparameter *simple-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (article noun))
+    (verb-phrase -> (verb noun-phrase))
+    (article -> the a)
+    (noun -> man ball woman table)
+    (verb -> hit took saw liked))
+  "A grammar for a trivial subset of English")
+
+(defvar *grammar* *simple-grammar*
+  "The grammar used by generate. Initially this is
+  *simple-grammar*, but we can switch to other grammars.")
+
+(defun rule-lhs (rule)
+  "The left hand side of the rule"
+  (first rule))
+
+(defun rule-rhs (rule)
+  "The right hand side of the rule"
+  (rest (rest rule)))
+
+(defun rewrites (category)
+  "Return list of possible rewrites for this category"
+  (rule-rhs (assoc category *grammar*)))
+
+(defun mappend (fn the-list)
+  "Apply fn to each element of list and append the results"
+  (apply #'append (mapcar fn the-list)))
+
+(defun generate (phrase)
+  "Generate random sentence or phrase"
+  (cond ((listp phrase)
+         (mappend #'generate phrase))
+        ((rewrites phrase)
+         (generate (random-elt (rewrites phrase))))
+        (T (list phrase))))
