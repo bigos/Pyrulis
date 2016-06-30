7c234ec86af1deb449796c9402a3600df447b2cb
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-30 11:56:25 +0100

    fun

--- Lisp/PAIP/chapter2.lisp
+++ Lisp/PAIP/chapter2.lisp
@@ -1,7 +1,7 @@
 (defun sentence ()    (append (noun-phrase) (verb-phrase)))
+(defun article ()     (one-of '(the a)))
 (defun noun-phrase () (append (article) (noun)))
 (defun verb-phrase () (append (verb) (noun-phrase)))
-(defun article ()     (one-of '(the a)))
 (defun noun ()        (one-of '(man woman table)))
 (defun verb ()        (one-of '(hit took saw liked)))
 
@@ -12,3 +12,18 @@
 (defun random-elt (choices)
   "Choose an element from a list at random"
   (elt choices (random (length choices))))
+
+(defun adj* ()
+  (if (= (random 2) 0)
+      nil
+      (append (adj) (adj*))))
+
+(defun pp* ()
+  (if (random-elt '(T nil))
+      (append (pp) (pp*))
+      nil))
+
+(defun noun-phrase () (append (article) (adj*) (noun) (pp*)))
+(defun pp () (append (prep) (noun-phrase)))
+(defun adj () (one-of '(big little blue green adiabatic)))
+(defun prep () (one-of '(to in by with on)))
