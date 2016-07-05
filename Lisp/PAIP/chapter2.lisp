41e773ebfcea4f8ce469194c0e2a82a4e8f89b65
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-05 13:12:16 +0100

    read up to page 79

--- Lisp/PAIP/chapter2.lisp
+++ Lisp/PAIP/chapter2.lisp
@@ -146,3 +146,13 @@
            ylist))
 
 ;;; 2.7 Exercises
+(defun cross-product (fn xlist ylist)
+  "Return a list of all (fn x y) values."
+  (mappend #'(lambda (y)
+               (mapcar #'(lambda (x) (funcall fn x y))
+                       xlist))
+           ylist))
+
+(defun combine-all (xlist ylist)
+  "Return a list of lists formed by appending a y to an x"
+  (cross-product #'append xlist ylist))
