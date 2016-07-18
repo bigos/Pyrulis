0cf91c2eab40aa66db129b825c02775286bd905d
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-19 00:03:16 +0100

    changed clojure version

--- Clojure/proba/project.clj
+++ Clojure/proba/project.clj
@@ -1,4 +1,4 @@
 (defproject proba "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main proba.core)
