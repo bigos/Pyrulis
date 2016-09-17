fdcb3191f169282b5db726894d76062cb11a911d
Jacek Podkanski <ruby.object@googlemail.com>
2016-09-17 15:26:37 +0100

    a bit of Haskell exercises

--- Haskell/HaskellProgrammingFromFirstPrinciples/scratchpad.hs
+++ Haskell/HaskellProgrammingFromFirstPrinciples/scratchpad.hs
@@ -1,1 +1,11 @@
-adder a b = a + b
+stops = "pbtdkg"
+vovels = "aeiou"
+
+nouns = ["cat", "dog", "mouse"]
+verbs = ["saw", "chased"]
+
+firstTup (a,_,_) = a
+
+combinations stops vovels = [(i,j,k) | i <- stops, j <-vovels, k <- stops]
+
+res s v = filter (\x -> firstTup x == 'p') $ combinations s v
