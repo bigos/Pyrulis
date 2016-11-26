59d5e722848b55c9e47c244f8dee8e24e38c062f
Jacek Podkanski <ruby.object@googlemail.com>
2016-11-26 17:11:32 +0000

    exercises

---
+++ Lisp/LispRoad/chapter1.lisp
@@ -0,0 +1,13 @@
+;;; define package for the chapter
+
+;;; p 5 example
+(defun divides (d n)
+  (eq (rem n d) 0))
+
+(defun ld (n)
+  (ldf 2 n))
+
+(defun ldf (k n)
+  (cond ((divides k n) k)
+        ((> (expt k 2) n) n)
+        (T (ldf (1+ k) n))))
