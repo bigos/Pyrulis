bcebdb250f012ed888b6c30d6bfd5718835ec129
Jacek Podkanski <ruby.object@googlemail.com>
2016-11-28 22:56:18 +0000

    little progress

--- Lisp/LispRoad/chapter1.lisp
+++ Lisp/LispRoad/chapter1.lisp
@@ -9,7 +9,16 @@
 
 (defun ldf (k n)
   (cond ((divides k n) k)
-        ((> (expt k 2) n) n)
+        ((> (* k k) n) n)
         (T (ldf (1+ k) n))))
 
 ;;; exercise 1.4 p 8
+
+(defun prime0 (n)
+  (cond ((< n 1) (error "not a positive integer"))
+        ((eq n 1) nil)
+        (T (eq (ld n) n))))
+
+;;; skipped lots of irrelevant stuff
+
+;;; page 13 playing the Haskell game
