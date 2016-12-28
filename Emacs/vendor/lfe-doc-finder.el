99f6306015f69572ec0bf19e57ed5c513b0a8d34
Jacek Podkanski <ruby.object@googlemail.com>
2016-12-28 19:01:25 +0000

    prepared for writing the code in a separate folder

---
+++ Emacs/vendor/lfe-doc-finder.el
@@ -0,0 +1,19 @@
+;;; lfe-doc-finder --- Code for finding LFE documentation
+
+;;; Commentary:
+
+;;; This is supposed to make easier to look up documentation
+
+;;; Usage:
+
+;;; M-x lfedoc
+
+;;; Code:
+
+(defun lfedoc ()
+  "Find LFE documentation."
+  (interactive)
+  (princ "going to look for LFE documentation"))
+
+(provide 'lfe-doc-finder)
+;;; lfe-doc-finder.el ends here
