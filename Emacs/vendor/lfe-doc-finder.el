95b8541f11bf8a68db99e4fd1ae6641d57256357
Jacek Podkanski <ruby.object@googlemail.com>
2016-12-28 20:17:51 +0000

    progress searching for the bracketed buffer substring

--- Emacs/vendor/lfe-doc-finder.el
+++ Emacs/vendor/lfe-doc-finder.el
@@ -13,7 +13,37 @@
 (defun lfedoc ()
   "Find LFE documentation."
   (interactive)
-  (princ "going to look for LFE documentation"))
+  (princ "going to look for LFE documentation")
+  (terpri)
+  ;; paredit-backward-up goes to the left of the opening bracket
+  ;; paredit-forward-up goes to the right of the closing bracket
+  (let ((original-buffer (current-buffer))
+        (original-marker (point-marker))
+        (opening-bracket)
+        (closing-bracket)
+        (bracketed))
+    (princ "original marker ")
+    (princ original-marker)
+    (princ " - ")
+    (princ (current-buffer))
+    (terpri)
+    (princ "zzzz")
+    (paredit-backward-up)
+    (setq opening-bracket (point-marker))
+    (forward-char)
+    (paredit-forward-up)
+    ;(backward-char)
+    (setq closing-bracket (point-marker))
+    (terpri)
+    (princ (cons opening-bracket closing-bracket))
+    (terpri)
+    (setq bracketed
+          (buffer-substring (marker-position opening-bracket)  (marker-position closing-bracket)))
+
+    (princ bracketed)
+    (goto-char (marker-position original-marker))
+    )
+  )
 
 (provide 'lfe-doc-finder)
 ;;; lfe-doc-finder.el ends here
