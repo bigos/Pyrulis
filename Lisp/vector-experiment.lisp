afff11eebaede58c1c8aa4bf7b192f1e2a31435b
Jacek Podkanski <ruby.object@googlemail.com>
2016-12-28 01:17:24 +0000

    bigger image with orange canvas

--- Lisp/vector-experiment.lisp
+++ Lisp/vector-experiment.lisp
@@ -10,25 +10,32 @@
 (in-package #:vecto-examples)
 
 (defun radiant-lambda (file)
-  (with-canvas (:width 90 :height 90)
+  (with-canvas (:width 900 :height 900)
     (let ((font (get-font (make-pathname
                            :directory "/usr/share/fonts/truetype/msttcorefonts"
                            :name "Verdana"
                            :type "ttf")))
-          (step (/ pi 7)))
+          (step (/ pi 6)))
+      (set-rgb-fill 1.0 0.65 0.3)
+      (rectangle 10 10 880 880)
+      (fill-path)
+
+      (set-rgb-fill 0.7 1.0 0.5)
       (set-font font 40)
-      (translate 45 45)
+      (translate 450 450)
       (draw-centered-string 0 -10 #(#x3BB))
+
       (set-rgb-stroke 1 0 0)
-      (centered-circle-path 0 0 35)
+      (centered-circle-path 0 0 400)
       (stroke)
+
       (set-rgba-stroke 0 0 1.0 0.5)
-      (set-line-width 4)
-      (dotimes (i 14)
+      (set-line-width 14)
+      (dotimes (i 12)
         (with-graphics-state
           (rotate (* i step))
-          (move-to 30 0)
-          (line-to 40 0)
+          (move-to 390 0)
+          (line-to 410 0)
           (stroke)))
       (save-png file))))
 
