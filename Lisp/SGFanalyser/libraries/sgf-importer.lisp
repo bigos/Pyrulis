a70ca9ea74bde2bd1621d234cb6fd0ae5c6c0dc1
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-29 00:26:22 +0100

    typed the grammar

--- Lisp/SGFanalyser/libraries/sgf-importer.lisp
+++ Lisp/SGFanalyser/libraries/sgf-importer.lisp
@@ -8,5 +8,43 @@
         "GN" "GC" "ON" "OT" "PB" "PC" "PW" "RE" "RO" "RU" "SO" "TM"
         "US" "WR" "WT" "BL" "OB" "OW" "WL" "FG" "PM" "VW" "HA" "KM" "TW" "TB"))
 
+
+(defun anything (char)
+  T)
+
+
+(defrule integer (+ (or "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
+  (:lambda (list)
+    (parse-integer (text list) :radix 10)))
+
+;;;-----------------------------------------------------------------------------
+(defrule s (+ game-tree))
+(defrule game-tree (and (* new-line) "(" (+ node) (* game-tree) ")" (* new-line)))
+(defrule node (and ";" (* data) (* new-line)))
+(defrule data (or (and "(" key (+ val) ")") move comment))
+(defrule comment (and "C" val-start text val-end))
+(defrule move (and (or "B" "W") val-start coordinates val-end))
+(defrule key (and uc-letter (? uc-letter)))
+(defrule val (and val-start c-value-type val-end (* new-line)))
+(defrule val-start "[")
+(defrule val-end "]")
+(defrule c-value-type (or value-type compose))
+(defrule value-type (or none text range))
+(defrule uc-letter (or "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M"
+                       "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"))
+(defrule compose (and value-type ":" value-type))
+(defrule none "")
+(defrule space #\space)
+(defrule new-line (or #\return (and #\linefeed (? #\return))))
+(defrule escaped-newline (and #\\ new-line))
+(defrule number (and (? (or "-" "+")) (+ integer)))
+(defrule text (+ (or new-line (anything character))))
+(defrule coordinate (or "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n"
+                        "o" "p" "q" "r" "s") )
+(defrule coordinates (and coordinate coordinate))
+(defrule range (and coordinates ":" coordinates))
+;;;-----------------------------------------------------------------------------
+
 (defun get-move-list (filename)
-  (let ((buffer (alexandria:read-file-into-string filename)))))
+  (let ((buffer (alexandria:read-file-into-string filename)))
+    buffer))
