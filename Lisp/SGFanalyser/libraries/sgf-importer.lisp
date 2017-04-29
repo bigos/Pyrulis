6d85a65381b8287f43d38584fdeb41d1a4ff76a9
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-29 13:35:05 +0100

    progress with parsing

--- Lisp/SGFanalyser/libraries/sgf-importer.lisp
+++ Lisp/SGFanalyser/libraries/sgf-importer.lisp
@@ -1,22 +1,7 @@
 (in-package :sgf-importer)
 
-(defun sgf-keys-list ()
-  (list "B" "W" "C" "N" "V"
-        "KO" "MN" "AB" "AE" "AW" "PL" "DM" "GB" "GW" "HO" "UC" "BM"
-        "DO" "IT" "TE" "AR" "CR" "DD" "LB" "LN" "MA" "SL" "SQ" "TR"
-        "AP" "CA" "FF" "GM" "ST" "SZ" "AN" "BR" "BT" "CP" "DT" "EV"
-        "GN" "GC" "ON" "OT" "PB" "PC" "PW" "RE" "RO" "RU" "SO" "TM"
-        "US" "WR" "WT" "BL" "OB" "OW" "WL" "FG" "PM" "VW" "HA" "KM" "TW" "TB"))
-
-
-(defun anything (char)
-  "except square brackets"
-  (cond
-    ((null char) nil)
-    ((eq char #\[) nil)
-    ((eq char #\]) nil)
-    (T T)))
-
+;; parsing documentation
+;; https://nikodemus.github.io/esrap/
 
 (defrule integer (+ (or "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
   (:lambda (list)
@@ -25,11 +10,11 @@
 ;;;-----------------------------------------------------------------------------
 (defrule s (+ game-tree))
 (defrule game-tree (and (* new-line) "(" (+ node) (* game-tree) ")" (* new-line)))
-(defrule node (and ";" (* data) (* new-line)))
-(defrule data (or (and  key val ) move comment))
+(defrule node (and ";" (* (or move (and  key val ) comment)) (* new-line)))
+
 (defrule comment (and "C" val-start text val-end))
 (defrule move (and (or "B" "W") val-start coordinates val-end))
-(defrule key (and uc-letter (? uc-letter)))
+(defrule key (and uc-letter (? uc-letter)) (:text T))
 (defrule val (and val-start c-value-type val-end (* new-line)))
 (defrule val-start "[")
 (defrule val-end "]")
@@ -43,7 +28,9 @@
 (defrule new-line (or #\return (and #\linefeed (? #\return))))
 (defrule escaped-newline (and #\\ new-line))
 (defrule number (and (? (or "-" "+")) (+ integer)))
-(defrule text (+ (anything character)))
+
+(defrule text (+  (not (or "[" "]"))) (:text T))
+
 (defrule coordinate (or "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n"
                         "o" "p" "q" "r" "s"))
 (defrule coordinates (and coordinate coordinate))
@@ -55,5 +42,10 @@
                                                   :external-format :latin-1)))
     buffer))
 
-(defun parseme ()
-  (parse 's (get-move-list "~/Documents/Go/Pro_collection/Cao Dayuan/cao_001.sgf")))
+(defun import-sgf (&optional (filename "~/Documents/Go/Pro_collection/Cao Dayuan/cao_001.sgf"))
+  (let* ((parsed-tree (cdar (parse 's (get-move-list filename))))
+         (stats (car (cdaadr parsed-tree)))
+         (stats-kv (loop for s in stats
+                      collect (cons (car s)
+                                    (elt (cadr s) 1)))))
+    stats-kv))
