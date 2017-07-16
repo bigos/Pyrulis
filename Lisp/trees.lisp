e3c682c0c4c623824c8e4d9f0b3017eba54abd26
Jacek Podkanski <ruby.object@googlemail.com>
2017-07-16 11:25:31 +0100

    cons version of a tree

---
+++ Lisp/trees.lisp
@@ -0,0 +1,13 @@
+;;; example from: http://shrager.org/llisp/13.html
+
+;;; cons versions
+
+(cons (cons 'mary
+            (cons 'had
+                  (cons 'a
+                        nil)))
+      (cons (cons 'little
+                  (cons (cons 'lambda
+                              nil)
+                        nil))
+            nil))
