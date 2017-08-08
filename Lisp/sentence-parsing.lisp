b72fdf0abe865fd9c991fc44696e140d30df861f
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-08 10:33:07 +0100

    sentence parsing stub

---
+++ Lisp/sentence-parsing.lisp
@@ -0,0 +1,3 @@
+;;; a toy for experimenting wth parsing text
+
+(defparameter sample-text "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.")
