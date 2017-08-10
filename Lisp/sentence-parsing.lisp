25a5ad4d7c4df535ea21d81518bbe826f874769d
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-10 02:17:54 +0100

    crude sentence diviosion works

--- Lisp/sentence-parsing.lisp
+++ Lisp/sentence-parsing.lisp
@@ -16,3 +16,30 @@
                         '(#\Space #\Newline #\, #\.))
         until res
         finally (return (when res index))))
+
+(defun findstart (seq start)
+  (loop for c across (subseq seq start)
+        for index = start then (1+ index)
+        for res = (notany (lambda (x) (eq c x))
+                        '(#\Space #\Newline #\, #\.))
+        until res
+        finally (return (when res index))))
+
+;;; mutually recursive functions
+(defun rfs (seq start acc)
+  (format t "rfs ~A ~A~%" start acc)
+  (if (null start)
+      (reverse acc)
+      (rfn seq (findstart seq start) (cons (findstart seq start) acc))))
+
+(defun rfn (seq start acc)
+  (format t "rfn ~A ~A~%" start acc)
+  (if (null start)
+      (reverse acc)
+      (rfs seq (findnext seq start) (cons (findnext seq start) acc))))
+
+(defun ws (seq)
+  (let ((ii (rfs seq 0 nil)))
+    (loop for a in ii
+          for b in (cdr ii)
+          collect (subseq seq a (or b (1+ a))))))
