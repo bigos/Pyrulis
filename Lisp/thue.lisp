bc5d154a3495297caded5e99e6785c5008770157
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-13 01:49:34 +0100

    binary addition of crude semi-thue system works

--- Lisp/thue.lisp
+++ Lisp/thue.lisp
@@ -16,7 +16,7 @@
         (reverse acc)
         (find-all s pattern (cons f acc)))))
 
-(defun replace-all (string part replacement &key (test #'char=))
+(defun replace-all-old (string part replacement &key (test #'char=))
   "Returns a new string in which all the occurences of the part
 is replaced with replacement."
   (with-output-to-string (out)
@@ -38,10 +38,32 @@ is replaced with replacement."
             (post (subseq str (+ patpos (length pat) 0))))
         (concatenate 'string pre replacement post)))))
 
-(defun replace-all-ng (str pat replacement)
+(defun replace-all (str pat replacement)
   (let ((patpos (search pat str)))
     (if (null patpos)
         str
         (let ((pre (subseq str 0 patpos))
               (post (subseq str (+ patpos (length pat)))))
-          (concatenate 'string pre replacement (replace-all-ng post pat replacement))))))
+          (concatenate 'string pre replacement (replace-all post pat replacement))))))
+
+(defvar *rules* '(("1_" . "1++")
+                  ("0_" . "1")
+                  ("01++" . "10")
+                  ("11++" . "1++0")
+                  ("_0" . "_")
+                  ("_1++" . "10")))
+
+(defvar *data* "_111_")
+
+(defun matching-rules (data)
+  (loop for r in *rules*
+        when (search (car r) data) collect r))
+
+(defun execute (data)
+  (format t "data ~s~%" data)
+  (let ((m (matching-rules data)))
+    (when m
+      (execute
+       (replace-all data
+                    (caar (matching-rules data))
+                    (cdar (matching-rules data)))))))
