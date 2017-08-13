e3cc1856e43c1a4473a1f0f9052db7c4f7cd95c5
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-13 01:55:52 +0100

    massive code cleanup to use only code needed to play with thue system

--- Lisp/thue.lisp
+++ Lisp/thue.lisp
@@ -1,43 +1,5 @@
 ;;; thue grammar toy
 
-(defstruct rule
-  lhs
-  rhs)
-
-(defun is-space (s)
-  (some (lambda (x)
-          (equalp x #\space))
-        s))
-
-(defun find-all (s pattern &optional acc)
-  (let ((f (search pattern s :start2 (1+ (or (car acc)
-                                             -1)))))
-    (if (null f)
-        (reverse acc)
-        (find-all s pattern (cons f acc)))))
-
-(defun replace-all-old (string part replacement &key (test #'char=))
-  "Returns a new string in which all the occurences of the part
-is replaced with replacement."
-  (with-output-to-string (out)
-    (loop with part-length = (length part)
-          for old-pos = 0 then (+ pos part-length)
-          for pos = (search part string
-                            :start2 old-pos
-                            :test test)
-          do (write-string string out
-                           :start old-pos
-                           :end (or pos (length string)))
-          when pos do (write-string replacement out)
-            while pos)))
-
-(defun replace-substring (str pat replacement)
-  (let ((patpos (search pat str)))
-    (when patpos
-      (let ((pre (subseq str 0 patpos))
-            (post (subseq str (+ patpos (length pat) 0))))
-        (concatenate 'string pre replacement post)))))
-
 (defun replace-all (str pat replacement)
   (let ((patpos (search pat str)))
     (if (null patpos)
@@ -59,6 +21,8 @@ is replaced with replacement."
   (loop for r in *rules*
         when (search (car r) data) collect r))
 
+;;; this is different from Thue language, because we apply the first rule found
+;;; the original uses random selected from possibly multiple matched rules
 (defun execute (data)
   (format t "data ~s~%" data)
   (let ((m (matching-rules data)))
