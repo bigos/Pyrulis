aaad4513c46ba26ecda5ffb2221503f3869bc49b
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-18 14:31:07 +0100

    simple experimenting

--- Lisp/simple-grammar.lisp
+++ Lisp/simple-grammar.lisp
@@ -1,5 +1,6 @@
 ;;; parsing ip numbers, arguments to grammar nodes are either seq or alt
-(defparameter better-grammar '((ip (seq num dot num dot num dot num))
+(defparameter better-grammar '((s (seq ip))
+                               (ip (seq num dot num dot num dot num))
                                (dot (alt
                                      #\.))
                                (num (seq dig dig dig))
@@ -14,3 +15,29 @@
                                      #\7
                                      #\8
                                      #\9))))
+
+(defun invalid-nodes (grammar)
+  (loop for p in grammar
+        for res = (and (symbolp (car p))
+                       (symbolp (caadr p))
+                       (> (length (cadr p)) 1))
+        unless res collect p))
+
+(defun find-node (grammar node)
+  (loop for p in grammar
+        for res = (equalp (car p) node)
+        until res
+        finally (return (when res p))))
+
+(defun node-value (grammar node)
+  (cadr (find-node grammar node)))
+
+(defun node-value-type (grammar node)
+  (car (node-value grammar node)))
+
+(defun find-character (grammar character)
+  (loop for p in grammar
+        for res = (cadr p)
+        when (some (lambda (x) (eq x character)) res)
+          collect (list (car p)
+                        (caadr p))))
