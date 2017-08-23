012d28d6b7d9dde11bcedc22ef579d3e0242eb23
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-23 18:03:40 +0100

    got rid of majority of reek warnings

--- Ruby/parsing.rb
+++ Ruby/parsing.rb
@@ -14,18 +14,22 @@ class Grammar
               digit: [:alt, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] }
   end
 
-  def path(s)
-    [s] + find_parents(s)
+  def path(symb)
+    [symb] + find_parents(symb)
   end
 
-  def find_parents(s)
-    parents = @data.select { |_, v| v.index s }.keys
+  def symbol_parent(symb)
+    @data.select { |_, val| val.index symb }.keys
+  end
+
+  def find_parents(symb)
+    parents = symbol_parent symb
     seen = []
     loop do
       break if parents.empty?
-      s = parents.first
-      seen << s
-      parents = @data.select { |_, v| v.index s }.keys
+      psym = parents.first
+      seen << psym
+      parents = symbol_parent psym
     end
     seen
   end
@@ -33,24 +37,25 @@ end
 
 # a class for creation of parse tree
 class ParseTree
+  attr_reader :tree
   def initialize(str)
     @grammar = Grammar.new
     @str = str
     @tree = parse
   end
 
-  def classify(c)
+  def classify(char)
     matches = []
     @grammar.data.each do |key, rhs|
-      matches << @grammar.path(key) unless rhs.reject { |x| x != c }.empty?
+      matches << @grammar.path(key).reverse unless rhs.reject { |elx| elx != char }.empty?
     end
     matches
   end
 
   def parse
     classified_chars = []
-    @str.each_char do |c|
-      classified_chars << [c, classify(c)]
+    @str.each_char do |char|
+      classified_chars << [char, classify(char)]
     end
 
     classified_chars
