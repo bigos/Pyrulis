90f452da9331d27fae16651e14bfebd59d65aa1d
Jacek Podkanski <ruby.object@googlemail.com>
2018-04-25 19:41:32 +0100

    code cleanup

--- Haskell/fizzbuzz.hs
+++ Haskell/fizzbuzz.hs
@@ -6,21 +6,20 @@ fizz x = mod x 3 == 0
 buzz :: Integral a => a -> Bool
 buzz x = mod x 5 == 0
 
--- results from to step
-results :: Integer -> Integer -> Integer -> [String]
-results f t s = map resulter [f, (f+s) .. t]
-
 checker :: Integral t => t -> [Bool]
 checker x = map (\fn -> fn x) [fizz, buzz]
 
 worder :: [Bool] -> [String]
-worder aa = zipWith (\a b -> if a then b else "" ) aa ["Fizz", "Buzz"]
+worder checkerApplied = zipWith (\a b -> if a then b else "" ) checkerApplied ["Fizz", "Buzz"]
 
 resulter :: (Show t, Integral t) => t -> String
 resulter x =
-  if ((worder(checker x)) == ["", ""])
+  if ((worder (checker x)) == ["", ""])
   then (show x)
   else (concat (worder (checker x)))
+
+results :: Integer -> Integer -> Integer -> [String]
+results from to step = map resulter [from, (from + step) .. to]
 
 main :: IO ()
 main = mapM_ (\x -> putStrLn x) (results 1 30 1)
