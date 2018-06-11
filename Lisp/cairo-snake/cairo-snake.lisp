d8f11347b97a942f112021ce31e1a8542be94394
Jacek Podkanski <ruby.object@googlemail.com>
2018-06-11 06:55:37 +0100

    searching for the callback problem solution

--- Lisp/cairo-snake/cairo-snake.lisp
+++ Lisp/cairo-snake/cairo-snake.lisp
@@ -9,39 +9,42 @@
 ;; key-press-fun
 
 (defun timer-fun (gm canvas)
-  nil)
+  T)
 
 (defun draw-fun (gm canvas context)
-  nil)
+  T)
 
 (defun key-press-fun (gm canvas rkv)
-  nil)
+
+  T)
 
 (defparameter global-model T)
 
 (defun main ()
   "Run the program"
+  (format t "entering main loop~%")
   (within-main-loop
     (let ((win (gtk-window-new :toplevel))
           (canvas (gtk-drawing-area-new)))
       (setf (gtk-window-default-size win) (list 300 200))
       (gtk-container-add win canvas)
 
-      (g-timeout-add 500 (timer-fun global-model canvas) :priority +g-priority-default+)
+      ;; (g-timeout-add 500 (timer-fun global-model canvas) :priority +g-priority-default+)
 
       ;; how do i use context here?
       (g-signal-connect canvas "draw"
-                        (lambda (context)
-                         (draw-fun global-model canvas context)))
+                        (lambda (canvas context)
+                          (draw-fun global-model canvas (pointer context))))
 
       ;; another untested copy
-      (g-signal-connect win "key-press"
-                        (lambda (rkv)
-                          (key-press-fun global-model canvas rkv)))
+      (g-signal-connect win "key-press-event"
+                        (lambda (win rkv)
+                          (key-press-fun global-model win rkv)))
 
       (g-signal-connect win "destroy"
-                        (lambda (widget)
-                          (declare (ignore widget))
+                        (lambda (win)
+                          (declare (ignore win))
                           (leave-gtk-main)))
 
-      (gtk-widget-show-all win))))
+      (gtk-widget-show-all win)))
+  (format t "~&after the main loop~%"))
