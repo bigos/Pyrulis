83a3cf1418d9ff6a06b903177ce7037a33510324
Jacek Podkanski <ruby.object@googlemail.com>
2018-12-28 17:14:40 +0000

    began adding source files

---
+++ Idris/CGlade/gladian.c
@@ -0,0 +1,23 @@
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+void some_signal_handler_func(GtkWidget *widget, gpointer user_data) {
+    /* do something useful here */
+}
+
+int main(int argc, char *argv[]) {
+    GladeXML *xml;
+
+    gtk_init(&argc, &argv);
+
+    /* load the interface */
+    xml = glade_xml_new("filename.glade", NULL, NULL);
+
+    /* connect the signals in the interface */
+    glade_xml_signal_autoconnect(xml);
+
+    /* start the event loop */
+    gtk_main();
+
+    return 0;
+}
