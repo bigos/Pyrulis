49fe4f29fb1909f84ed6607532b3da8cee875e43
Jacek Podkanski <ruby.object@googlemail.com>
2018-12-29 23:07:51 +0000

    gui example works

--- Idris/CGlade/gladian.c
+++ Idris/CGlade/gladian.c
@@ -1,23 +1,50 @@
 #include <gtk/gtk.h>
-#include <gladeui/glade.h>
 
-void some_signal_handler_func(GtkWidget *widget, gpointer user_data) {
-    /* do something useful here */
+/*
+   https://developer.gnome.org/gtk3/stable/ch01s03.html
+*/
+
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
 }
 
-/* dead end - lack of contemporary tutorials */
-int main(int argc, char *argv[]) {
-    struct GladeProject *gp;
+int
+main (int   argc,
+      char *argv[])
+{
+  GtkBuilder *builder;
+  GObject *window;
+  GObject *button;
+  GError *error = NULL;
 
-    gtk_init(&argc, &argv);
+  gtk_init (&argc, &argv);
 
-    gp = glade_project_new();
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder = gtk_builder_new ();
+  if (gtk_builder_add_from_file (builder, "/home/jacek/Programming/Pyrulis/Idris/CGlade/builder.ui", &error) == 0)
+    {
+      g_printerr ("Error loading file: %s\n", error->message);
+      g_clear_error (&error);
+      return 1;
+    }
 
-    /* load the interface */
-    glade_project_load_from_file(*gp, "/home/jacek/Programming/Pyrulis/Idris/CGlade/gladian.glade");
+  /* Connect signal handlers to the constructed widgets. */
+  window = gtk_builder_get_object (builder, "window");
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-    /* start the event loop */
-    gtk_main();
+  button = gtk_builder_get_object (builder, "button1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
 
-    return 0;
+  button = gtk_builder_get_object (builder, "button2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  button = gtk_builder_get_object (builder, "quit");
+  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+  gtk_main ();
+
+  return 0;
 }
