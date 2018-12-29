6a31307a37d84608f63a8a159ea6e06b2be51fb2
Jacek Podkanski <ruby.object@googlemail.com>
2018-12-29 00:38:30 +0000

    dead end - lack of contemporary tutorials

--- Idris/CGlade/gladian.c
+++ Idris/CGlade/gladian.c
@@ -5,16 +5,16 @@ void some_signal_handler_func(GtkWidget *widget, gpointer user_data) {
     /* do something useful here */
 }
 
+/* dead end - lack of contemporary tutorials */
 int main(int argc, char *argv[]) {
-    GladeXML *xml;
+    struct GladeProject *gp;
 
     gtk_init(&argc, &argv);
 
-    /* load the interface */
-    xml = glade_xml_new("filename.glade", NULL, NULL);
+    gp = glade_project_new();
 
-    /* connect the signals in the interface */
-    glade_xml_signal_autoconnect(xml);
+    /* load the interface */
+    glade_project_load_from_file(*gp, "/home/jacek/Programming/Pyrulis/Idris/CGlade/gladian.glade");
 
     /* start the event loop */
     gtk_main();
