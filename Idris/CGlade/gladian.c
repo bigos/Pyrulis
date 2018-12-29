a78a166ed9c3ccc362a755579e9123ff179398c2
Jacek Podkanski <ruby.object@googlemail.com>
2018-12-29 23:59:01 +0000

    new gtk builder example somewhat works

--- Idris/CGlade/gladian.c
+++ Idris/CGlade/gladian.c
@@ -4,12 +4,6 @@
    https://developer.gnome.org/gtk3/stable/ch01s03.html
 */
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
-}
 
 int
 main (int   argc,
@@ -17,34 +11,16 @@ main (int   argc,
 {
   GtkBuilder *builder;
   GObject *window;
-  GObject *button;
-  GError *error = NULL;
 
   gtk_init (&argc, &argv);
 
   /* Construct a GtkBuilder instance and load our UI description */
-  builder = gtk_builder_new ();
-  if (gtk_builder_add_from_file (builder, "/home/jacek/Programming/Pyrulis/Idris/CGlade/builder.ui", &error) == 0)
-    {
-      g_printerr ("Error loading file: %s\n", error->message);
-      g_clear_error (&error);
-      return 1;
-    }
+  builder = gtk_builder_new_from_file("/home/jacek/Programming/Pyrulis/Idris/CGlade/gladian.glade");
+  window = gtk_builder_get_object(builder, "main-window");
+  gtk_builder_connect_signals(builder, NULL);
+  g_object_unref(builder);
 
-  /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object (builder, "window");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
-  button = gtk_builder_get_object (builder, "button1");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "button2");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-
-  button = gtk_builder_get_object (builder, "quit");
-  g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
-
+  gtk_widget_show(window);
   gtk_main ();
-
   return 0;
 }
