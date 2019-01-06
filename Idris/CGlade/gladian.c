c06d1e013479c41d87ab9a03cc415b3cc4016a57
Jacek Podkanski <ruby.object@googlemail.com>
2019-01-06 18:12:35 +0000

    copied good files

---
+++ Idris/CGlade/gladian.c
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+
+/*
+   https://developer.gnome.org/gtk3/stable/GtkBuilder.html
+   https://de.wikibooks.org/wiki/GTK_mit_Builder:_Builder
+   https://developer.gnome.org/gnome-devel-demos/stable/c.html.en
+*/
+
+int
+foo (int   argc,
+      char ** argv)
+{
+  GtkBuilder *builder;
+
+  GtkWidget *window;
+
+  gtk_init (&argc, &argv);
+
+  /* Construct a GtkBuilder instance and load our UI description */
+  builder = gtk_builder_new_from_file("/home/jacek/Programming/Pyrulis/Idris/CGlade/gladian.glade");
+
+  window = GTK_WIDGET(gtk_builder_get_object (builder, "main-window"));
+  gtk_builder_connect_signals(builder, NULL);
+  g_object_unref(builder);
+
+  gtk_widget_show_all(window);
+
+  gtk_main ();
+  return 0;
+}
