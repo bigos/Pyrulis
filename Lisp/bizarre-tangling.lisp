f8b33cb51eb7012e28efac9c5a8040edf25e992e
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-11 22:40:18 +0000

    bizarre tangling

---
+++ Lisp/bizarre-tangling.lisp
@@ -0,0 +1,25 @@
+#+PROPERTY: header-args :tangle yes
+
+* Header 1
+This Emacs code pasted in the scratch buffer and executed adds a cool tangling
+function.
+
+#+BEGIN_EXAMPLE
+(defun bizarro-tangle (comment-string)
+  "Comment out every line but src blocks in chosen syntax"
+  (interactive "sComment string: ")
+  (beginning-of-buffer)
+  (while (not (eobp))
+    (unless (org-in-src-block-p t)   ; comment out BEGIN and END lines
+      (save-excursion (beginning-of-line)
+              (insert comment-string)))
+    (next-line)))
+#+END_EXAMPLE
+
+** code
+
+will go here
+
+#+BEGIN_SRC common-lisp
+  (format t "hello~%")
+#+END_SRC
