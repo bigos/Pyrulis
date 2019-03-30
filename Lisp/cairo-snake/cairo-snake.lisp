4619060e8d33f31ce7a9039da51360f33760b74d
Jacek Podkanski <ruby.object@googlemail.com>
2019-03-30 22:55:36 +0000

    commenting out the code shows the window

--- Lisp/cairo-snake/cairo-snake.lisp
+++ Lisp/cairo-snake/cairo-snake.lisp
@@ -290,27 +290,27 @@
   (format *o* "global model is ~A~%" global-model)
   (within-main-loop
     (let ((win (gtk-window-new :toplevel))
-          (canvas (gtk-drawing-area-new)))
+          ;; (canvas (gtk-drawing-area-new))
+          )
 
       (setf (gtk-window-default-size win) (list 300 200))
-      (gtk-container-add win canvas)
+      ;; (gtk-container-add win canvas)
 
-      (g-timeout-add 1000
-                     (lambda () (timer-fun global-model canvas))
-                     :priority +g-priority-default+)
+      ;; (g-timeout-add 1000
+      ;;                (lambda () (timer-fun global-model canvas))
+      ;;                :priority +g-priority-default+)
 
-      (g-signal-connect canvas "draw"
-                        (lambda (canvas context)
-                          (draw-fun global-model canvas context)))
+      ;; (g-signal-connect canvas "draw"
+      ;;                   (lambda (canvas context)
+      ;;                     (draw-fun global-model canvas context)))
 
-      (g-signal-connect win "key-press-event"
-                        (lambda (win rkv)
-                          (key-press-fun global-model win rkv)))
+      ;; (g-signal-connect win "key-press-event"
+      ;;                   (lambda (win rkv)
+      ;;                     (key-press-fun global-model win rkv)))
 
       (g-signal-connect win "destroy"
                         (lambda (win)
                           (declare (ignore win))
-                          (format *o* "~&going to leave")
                           (leave-gtk-main)))
 
       (gtk-widget-show-all win)))
