850c4a07492f3349acdf5a141a671a6216504428
Jacek Podkanski <ruby.object@googlemail.com>
2019-07-08 00:14:35 +0100

    lots of warnings but the window shows on the screen

--- Idris/CGlade/Main.idr
+++ Idris/CGlade/Main.idr
@@ -1,25 +1,25 @@
 module Main
 
-  -- same as " `pkg-config --libs gtk+-3.0`
-  %flag C "-lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0"
-  %include C "myc.h"
-  %link C "myc.o"
+-- same as " `pkg-config --libs gtk+-3.0`
+%flag C "-lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0"
+%include C "myc.h"
+%link C "myc.o"
 
-  myCallback : Int -> Int
-  myCallback a = a * 5
+myCallback : Int -> Int
+myCallback a = a * 5
 
-  myMult     : Int -> Int -> IO Int
-  myMult x y = foreign FFI_C "myMult"
+myMult     : Int -> Int -> IO Int
+myMult x y = foreign FFI_C "myMult"
   (Int -> Int -> CFnPtr (Int -> Int) -> IO Int)
   x y (MkCFnPtr myCallback)
 
-  zzz : IO Int
-  zzz = foreign FFI_C "zzz"
+zzz : IO Int
+zzz = foreign FFI_C "zzz"
   (IO Int)
 
 
-  main : IO ()
-  main = do
+main : IO ()
+main = do
   r <- myMult 5 4
   putStrLn ("doubled " ++ (show r))
   s <- zzz
---
+++ Idris/CGlade/myc.c
@@ -0,0 +1,54 @@
+#include <gtk/gtk.h>
+
+int myMult(int x, int y, int (* cba)()) {
+    int res = x * y;
+    printf("%d\n", res);
+
+    printf("callbacked %d\n", (cba)(3));
+
+    return (res * 2);
+}
+
+
+int zzz ()
+{
+    int status = 0;
+
+    int argc = 0;
+    char **argv = (char *[]){"hello", "world"};
+
+    GtkBuilder *builder;
+    GtkWidget  *window;
+    GError     *error = NULL;
+
+    /* Init Gtk3 */
+    gtk_init( &argc, &argv);
+
+    /* Create new GtkBuilder object */
+    builder = gtk_builder_new();
+    /* Load UI from file. If error occurs, report it and quit application.
+     * Replace "tut.glade" with your saved project. */
+    if( ! gtk_builder_add_from_file( builder, "ui.glade", &error ) )
+    {
+        g_warning( "%s", error->message );
+        g_free( error );
+        return( 1 );
+    }
+
+    /* Get main window pointer from UI */
+    window = GTK_WIDGET( gtk_builder_get_object( builder, "w1" ) );
+
+    /* Connect signals */
+    gtk_builder_connect_signals( builder, NULL );
+
+    /* Destroy builder, since we don't need it anymore */
+    g_object_unref( G_OBJECT( builder ) );
+
+    /* Show window. All other widgets are automatically shown by GtkBuilder */
+    gtk_widget_show( window );
+
+    /* Start main loop */
+    gtk_main();
+
+    return status;
+}
