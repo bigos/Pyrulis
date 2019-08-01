d4e99d5060307bcf8a26e0a072467daf7fb5f7b8
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-01 19:04:32 +0100

    shared library produces window

--- C/Linux/GtkTest/foo.c
+++ C/Linux/GtkTest/foo.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 
 void foo(void)
 {
     puts("Hello, I am a shared library");
+    mainz();
+}
+
+
+
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
+    GtkWidget *window;
+
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all (window);
+}
+
+int
+mainz (void)
+{
+    int    argc = 0;
+    char **argv = 0;
+
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
 }
--- C/Linux/GtkTest/foo.h
+++ C/Linux/GtkTest/foo.h
@@ -1,1 +1,2 @@
 void foo(void);
+int mainz(void);
