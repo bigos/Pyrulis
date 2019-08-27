eddbd645044b5fa411026a9afa1262d553faa21a
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-27 19:53:48 +0100

    trying vala

--- Lisp/new-gtk-project.lisp
+++ Lisp/new-gtk-project.lisp
@@ -7,12 +7,15 @@
   (read-line))
 
 (defun main ()
-  (let* ((npn (prompt-input "Enter project name" )))
-    (format t "Creating project ~A~%" npn)
+  (let* ((new-project-name (prompt-input "Enter project name" )))
+    (format t "Creating project ~A~%" new-project-name)
 
     (setf quickproject:*author* "Jacek Podkanski")
     (setf quickproject:*license* "Public Domain")
     (setf quickproject:*include-copyright* nil)
 
-    (quickproject:make-project  (merge-pathnames #p "~/Programming/Lisp/" npn)
+    (quickproject:make-project  (merge-pathnames #p "~/Programming/Lisp/"
+                                                 new-project-name)
                                 :depends-on '(:cl-cffi-gtk :alexandria))))
+
+(main)
