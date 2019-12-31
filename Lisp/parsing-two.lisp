4f90639c838c480a6851076b04886f69df4490d4
Jacek Podkanski <ruby.object@googlemail.com>
2019-12-31 00:45:18 +0000

    repeat-consume

--- Lisp/parsing-two.lisp
+++ Lisp/parsing-two.lisp
@@ -7,13 +7,14 @@
 (defpackage #:parsing-two
   (:use #:cl))
 
-(in-package #:parsing-two)              ;---------------------------------------
+;;; --- put cursor at the end of the next s-expr and press s-e to copy to REPL -
+(in-package #:parsing-two)
 
 (defun data ()
   (format nil " 123 + 456 # comment"))
 
 ;;; character extraction
-
+(proclaim `(ftype (function ((simple-array character) fixnum) character) at))
 (defun at (string index)
   "STRING character at index"
   (aref string index))
@@ -28,3 +29,19 @@
       (if (funcall (first predicates) (at data index))
           (list 'ok (at data index))
           (consume data index (cdr predicates)))))
+
+;; (repeat-consume (data) 13 nil (list 'alpha-char-p) nil)
+(defun repeat-consume (data index count predicates acc)
+  "call consume function at most COUNT times or to the end of the DATA
+  if COUNT is nil"
+  (if (or (when count (zerop count))
+          (>= index (length data)))
+      (list 'ok (reverse acc))
+      (if (eq 'ok (car (consume data index predicates)))
+          (repeat-consume data
+                          (1+ index)
+                          (when count (1- count))
+                          predicates
+                          (cons (at data index)
+                                acc))
+          (list 'error (at data index)))))
