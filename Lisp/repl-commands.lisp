62512e8ba527aef40ce85f3af83435f0f3a28f38
Jacek Podkanski <ruby.object@googlemail.com>
2020-11-27 00:55:46 +0000

    simplified some code and added some more

--- Lisp/repl-commands.lisp
+++ Lisp/repl-commands.lisp
@@ -1,15 +1,16 @@
-;;; toy lisp
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (ql:quickload '(draw-cons-tree)))
 
 (defun dt (l)
   (draw-cons-tree:draw-tree l))
 
-;;; ============================================================================
+;;; === keyboard shortcuts =====================================================
 
 (defun keys-shortcuts-hash ()
   (let ((key-hash (make-hash-table :test 'equalp)))
-    (loop for kv in '(("h" . com-help)
+    (loop for kv in '(("i" . com-init)
+                      ("p" . com-print)
+                      ("h" . com-help)
                       ("quit" . com-quit))
 
           do (setf (gethash (car kv) key-hash) (cdr kv)))
@@ -23,7 +24,18 @@
 (defun keys-command (key)
   (gethash key *keys*))
 
-;;; ============================================================================
+;;; === model ==================================================================
+
+(defparameter *model* nil)
+
+;;; === commands ===============================================================
+
+(defun com-init ()
+  (setf *model* (list 'ala 'ma 'kota)))
+
+(defun com-print ()
+  (format t "=== ~A~%" *model*)
+  *model*)
 
 (defun com-help ()
   (format t "key  command ----~%")
@@ -31,22 +43,19 @@
         do (format t "~A ~A~%" x y)))
 
 (defun com-quit ()
-  (format t "doing quitting~%~%")
+  (format t "quitting now...~%~%")
   'quit-main-loop)
 
-;;; ============================================================================
+;;; === loop ===================================================================
 
 (defun my-loop ()
   (let ((prompt "~&~% > "))
     (format t prompt)
     (loop for input = (read-line)
-          until (eq (process input) 'quit-main-loop)
+          until (eq 'quit-main-loop     ;com-quit returns 'quit-main-loop
+                    (let ((command (keys-command input)))
+                      (if command
+                          (funcall command)
+                          (format t "unrecognised command entered~%"))))
           do
              (format t prompt))))
-
-(defun process (input)
-  (let ((command (keys-command input)))
-    (cond (command
-           (funcall command)) ;; when input is quit 'quit-main-loop will return
-          (T
-           (format t "unrecognised command entered~%")))))
