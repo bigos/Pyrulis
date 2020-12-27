1c497ac50443d94bbe98267c898577b6243cb890
Jacek Podkanski <ruby.object@googlemail.com>
2020-12-27 00:46:30 +0000

    range

--- Lisp/hello.lisp
+++ Lisp/hello.lisp
@@ -1,3 +1,13 @@
 #! /usr/bin/sbcl --script
 
 (format T "Hello World~%")
+
+(defun range (start end)
+  (labels ((range2 (s e a)
+             (if (eq s (car a))
+                 a
+                 (range2 s e (cons (if (> s e)
+                                       (1+ (car a))
+                                       (1- (car a)))
+                                   a)))))
+    (range2 start end (list end))))
