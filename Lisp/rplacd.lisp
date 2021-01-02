a810f49333640ddc4dd22bd2b2841784b745af59
Jacek Podkanski <ruby.object@googlemail.com>
2021-01-02 00:57:54 +0000

    connections

--- Lisp/rplacd.lisp
+++ Lisp/rplacd.lisp
@@ -10,17 +10,32 @@
                       (zn_plus "d" na3)
                       (zn_minus "d" na3)))
 
-(defparameter gr nil)
+(defparameter *gr* nil)
 
 (defun push-links (links)
   (loop for l in links do
-    (let ((av (assoc (car l) gr)))
+    (let ((av (assoc (car l) *gr*)))
       (if av
           (rplacd av (cons (cdr l)
                            (cdr av)))
           (push (list (car l) (cdr l))
-                gr)))))
+                *gr*)))))
+
+(defun links-leaving (node-name)
+  (loop for l in (cdr (assoc node-name *gr*)) collect (car l)))
+
+(defun links-coming (node-name)
+  (loop for n in *gr*
+        for l = (loop for ln in (cdr n)
+                      when (equalp node-name (cadr ln)) collect ln)
+        when l collect (list (car n) l)))
 
 (progn
-  (setf gr nil)
-  (push-links links))
+  (format t "=====~%")
+  (setf *gr* nil)
+  (push-links links)
+  (format t "~A~%" *gr*)
+  (format t "=====~%")
+  (format t "~A~%" (links-leaving 'b3))
+  (format t "=====~%")
+  (format t "~A~%" (links-coming 'na3)))
