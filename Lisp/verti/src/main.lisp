28caa8318e58154f8f911d468e26f8215419d62d
Jacek Podkanski <ruby.object@googlemail.com>
2021-02-05 23:18:41 +0000

    vert

--- Lisp/verti/src/main.lisp
+++ Lisp/verti/src/main.lisp
@@ -2,4 +2,13 @@
   (:use :cl))
 (in-package :verti)
 
-;; blah blah blah.
+(defun describe-instance (inst)
+  (cons (type-of inst)
+        (loop for slot in (sb-mop:class-direct-slots (class-of inst))
+              collect (list
+                       (sb-mop:slot-definition-name slot)
+                       (slot-value inst (sb-mop:slot-definition-name slot))))))
+(defstruct vert
+  (source)
+  (action)
+  (target))
