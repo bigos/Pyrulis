3c96ed00e8245ff2eaecef39671c789a43355482
Jacek Podkanski <ruby.object@googlemail.com>
2021-03-28 23:22:20 +0100

    example of using dot-cons-tree

--- Lisp/Closer/timer.lisp
+++ Lisp/Closer/timer.lisp
@@ -1,4 +1,12 @@
 ;;; oo timer program will go here
+(declaim (optimize (speed 0) (space 0) (safety 2) (debug 3)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload '(alexandria serapeum dot-cons-tree fiveam)))
+
+(defun dd (l)
+  (dot-cons-tree:draw-graph l))
+
 (format t "~%this is a timer~%~%")
 
 (defclass my-timer ()
@@ -19,3 +27,12 @@
 (format t "initial value is: ~d~%" (get-value *obj*))
 
 (format t "increased value is: ~d~%" (inc *obj*))
+
+(dot-cons-tree:draw-graph (list 1 *obj* 3 4 5))
+
+
+(defun testme ()
+  (5am:test abc
+    (5am:is (= 1 2)))
+
+  (5am:run! 'abc))
