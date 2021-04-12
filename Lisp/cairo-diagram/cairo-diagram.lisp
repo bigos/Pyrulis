d7162cc187199487e4c512a8def181c3ca4290f3
Jacek Podkanski <ruby.object@googlemail.com>
2021-04-12 02:18:25 +0100

    3 dots

--- Lisp/cairo-diagram/cairo-diagram.lisp
+++ Lisp/cairo-diagram/cairo-diagram.lisp
@@ -14,18 +14,59 @@
 (defun init-global-model ()
   (setf *global* nil))
 
-(defun key-press-fun (gm canvas rkv)
+(defun draw-canvas (canvas context)
+  (let* ((w (gtk-widget-get-allocated-width canvas))
+         (h (gtk-widget-get-allocated-height canvas))
+         (size (cons w h))
+         (s 99999)
+
+         (cr (pointer context)))
+
+    ;; prevent cr being destroyed improperly
+    (cairo-reference cr)
+
+    (format *o* "canvas size is ~A --- ~A ~A === ~A~%" size w h s)
+    (format *o* "pointer context ~A~%" cr)
+
+    (cairo-set-source-rgb cr 0.6 0.9 0)
+    (cairo-set-line-width cr 25)
+    (cairo-set-line-cap cr :round)
+    (cairo-set-line-join cr :round)
+
+    ;; draw dots
+    (cairo-set-source-rgb cr 0.4 0.6 0.1)
+    (cairo-set-line-width cr 13)
+    (loop for c in (list '(0 . 0) '(10 . 1) '(20 . 2))
+          do (cairo-move-to cr (* 10 (car c)) (* 10 (cdr c)))
+             (cairo-line-to cr (* 10 (car c)) (* 10 (cdr c))))
+    (cairo-stroke cr)
+
+
+    ;; cleanup
+    ;; cairo destroy must have matching cairo-reference
+    (cairo-destroy cr)
+
+    ;; continue propagation of the event handler
+    +gdk-event-propagate+))
+
+(defun draw-fun (canvas context)
+  (draw-canvas canvas context))
+
+(defun key-press-fun (canvas rkv)
   (format *o* "key press fun ~A ~A~%" canvas rkv)
   (let ((kv (gdk-event-key-keyval rkv)))
 
-    (format *o* "key value ~A ~A~%" kv gm)))
+    (format *o* "key value ~A~%" kv)))
+
+
+(defun timer-fun (canvas)
+  ;; (format *o* "AFTER timer fun ~A~%" gm)  ;problem here
+  (not nil))
 
 (defun main ()
   "Run the program"
   (format *o* "boooo~%")
   (format t "entering main loop~%")
-  (init-global-model)
-  (format *o* "global model is ~A~%" *global*)
 
   (sb-int:with-float-traps-masked (:divide-by-zero)
     (within-main-loop
@@ -36,17 +77,17 @@
        (setf (gtk-window-default-size win) (list 300 200))
        (gtk-container-add win canvas)
 
-       ;; (g-timeout-add 1000
-       ;;                (lambda () (timer-fun global-model canvas))
-       ;;                :priority +g-priority-default+)
+       (g-timeout-add 1000
+                      (lambda () (timer-fun canvas))
+                      :priority +g-priority-default+)
 
-       ;; (g-signal-connect canvas "draw"
-       ;;                   (lambda (canvas context)
-       ;;                     (draw-fun global-model canvas context)))
+       (g-signal-connect canvas "draw"
+                         (lambda (canvas context)
+                           (draw-fun canvas context)))
 
        (g-signal-connect win "key-press-event"
                          (lambda (win rkv)
-                           (key-press-fun global-model win rkv)))
+                           (key-press-fun win rkv)))
 
        (g-signal-connect win "destroy"
                          (lambda (win)
