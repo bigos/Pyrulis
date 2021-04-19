52484265e4765ef3371342a388d5f903f4f115ee
Jacek Podkanski <ruby.object@googlemail.com>
2021-04-20 00:23:20 +0100

    nested list as input to graph

--- Lisp/fistma-graph.lisp
+++ Lisp/fistma-graph.lisp
@@ -1,29 +1,64 @@
-(declaim (optimize (speed 1) (debug 0)))
+(declaim (optimize (speed 0) (debug 3)))
 
 #| loading
 (load "~/Programming/Pyrulis/Lisp/fistma-graph.lisp")
 (in-package #:fistma-graph)
 (draw-graph *graph*)
+(draw-graph (dot-links *nested*))
 |#
 
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload '(serapeum)))
+  (ql:quickload '(alexandria serapeum dot-cons-tree)))
 
 (defpackage #:fistma-graph
   (:use #:cl)
-  (:local-nicknames (#:se #:serapeum)))
+  (:local-nicknames (#:se #:serapeum) (#:ax #:alexandria)))
 
 (in-package #:fistma-graph)
 
 (defparameter *graph*
   ;; source action target
-  '(
-    (closed lock locked)
+  '((closed lock locked)
     (closed open opened)
-    (locked "un lock" closed)
-    (opened close closed)
+    (locked "unlock" closed)
+    (opened close closed)))
 
-    ))
+(defparameter *nested*
+  ;; (source (action target))
+  '((closed
+     (lock locked)
+     (open opened))
+    (locked
+     (unlock closed))
+    (opened
+     (close closed))))
+
+(defun dot-links (l)
+  "Take a nested graph L and convert it to list of links for draw-graph."
+  (let ((a))
+    (loop for s in l do
+      (unless (atom (car s)) (error "Source must be an atom"))
+      (loop for at in (cdr s) do
+        (unless (atom (car  at)) (error "Action must be an atom"))
+        (unless (atom (cadr at)) (error "Target must be an atom"))
+        (push (cons (car s) at) a)))
+    (reverse a)))
+
+(defun sources ()
+  (mapcar #'car *nested*))
+
+(defun actions ()
+  (ax:flatten
+   (mapcar
+    (lambda (x) (mapcar #'car (cdr x)))
+    *nested*)))
+
+(defun targets ()
+  (ax:flatten
+   (mapcar
+    (lambda (x) (mapcar #'cdr (cdr x)))
+    *nested*)))
 
 (defun prepare-graph (data)
   "Take DATA and prepare a graph string."
