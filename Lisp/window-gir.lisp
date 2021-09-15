fcfd6923ac9e9abbdc1d7dac8ebb63c741b9c216
Jacek Podkanski <ruby.object@googlemail.com>
2021-09-15 23:37:53 +0100

    first signal

--- Lisp/window-gir.lisp
+++ Lisp/window-gir.lisp
@@ -16,7 +16,16 @@
 (let ((window (gir:invoke (*gtk* "Window" 'new)
                           (gir:nget *gtk* "WindowType" :toplevel))))
 
+  ;; https://github.com/andy128k/cl-gobject-introspection/blob/d0136c8d9ade2560123af1fc55bbf70d2e3db539/examples/maze/src/gui/drawing.lisp#L27
   (setf (gir:property window 'title) "This is GIR")
+  (gir:invoke (window "set_default_size")
+              400 300)
+
+  (gir:connect window :destroy
+               (lambda (win)
+                 (declare (ignore win))
+                 (format t "~&pressed close widget~%")
+                 (gir:invoke (*gtk* 'main-quit))))
 
   (gir:invoke (window 'show))
   (gir:invoke (*gtk* 'main)))
