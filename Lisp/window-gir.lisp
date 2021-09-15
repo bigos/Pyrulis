d908d1fb728daee5ba24233b2a9677e1b0d2776a
Jacek Podkanski <ruby.object@googlemail.com>
2021-09-15 23:28:49 +0100

    setting the title

---
+++ Lisp/window-gir.lisp
@@ -0,0 +1,22 @@
+;; https://github.com/andy128k/cl-gobject-introspection
+;; https://github.com/andy128k/cl-gobject-introspection/blob/master/examples/flood-game/load.lisp
+;; https://github.com/andy128k/cl-gobject-introspection/blob/d0136c8d9ade2560123af1fc55bbf70d2e3db539/examples/flood-game/src/glib-timeout-add.lisp#L18
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload '(:alexandria :serapeum :cffi :cl-gobject-introspection :cl-cairo2)))
+
+(defpackage #:window-gir
+  (:use #:cl))
+
+;; (load "/home/jacek/Programming/Pyrulis/Lisp/window-gir.lisp")
+(in-package :window-gir)
+
+(defvar *gtk* (gir:require-namespace "Gtk"))
+(gir:invoke (*gtk* 'init) nil)
+(let ((window (gir:invoke (*gtk* "Window" 'new)
+                          (gir:nget *gtk* "WindowType" :toplevel))))
+
+  (setf (gir:property window 'title) "This is GIR")
+
+  (gir:invoke (window 'show))
+  (gir:invoke (*gtk* 'main)))
