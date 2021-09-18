bd6c6b6078b0551b1b16c75e334f716fa7574d49
Jacek Podkanski <ruby.object@googlemail.com>
2021-09-19 00:20:37 +0100

    extracting some information

--- Lisp/window-gir-app.lisp
+++ Lisp/window-gir-app.lisp
@@ -2,7 +2,7 @@
 ;;; https://github.com/andy128k/cl-gobject-introspection
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload '(:alexandria :serapeum :cffi :cl-gobject-introspection :cl-cairo2)))
+  (ql:quickload '(:alexandria :serapeum :cffi :cl-gobject-introspection :cl-cairo2 :s-xml)))
 
 (defpackage #:window-gir-app
   (:use #:cl))
@@ -12,6 +12,34 @@
 (in-package :window-gir-app)
 
 (defvar *gtk* (gir:require-namespace "Gtk"))
+
+(defun path-gir-gtk ()
+  #p"/usr/share/gir-1.0/Gtk-3.0.gir")
+
+(defun parsed-gir-gtk ()
+  (cdr
+   (nth 8 (s-xml:parse-xml-file (path-gir-gtk)))))
+
+(defun parsed-kind-names ()
+  (remove-duplicates  (loop for el in (parsed-gir-gtk)
+                            collect (caar el))
+                      :test #'equalp))
+
+;; (parsed-kinds 'ns-0:|bitfield|)
+(defun parsed-kinds (symb)
+  (loop for el in (parsed-gir-gtk)
+        when (equalp symb (caar el)) collect el))
+
+(defun parsed-classes ()
+  (loop for el in (parsed-gir-gtk)
+        when (equalp 'ns-0:|class| (caar el)) collect (caddr (car el))))
+
+;; usage (gir-find "ApplicationWindow")
+(defun gir-find (str)
+  (loop
+    for el in (parsed-gir-gtk)
+    when (search str (caddr (car el)))
+      collect el))
 
 (defun activate (w)
   (declare (ignore w))
@@ -40,6 +68,8 @@
     (gir:connect app :activate
                  (lambda (w)
                    (activate w)))
+
+    (break "aaa")
 
     (let ((status (gir:invoke (app "g_application_run"))))
       (gir::g-object-unref (cffi:make-pointer app))
