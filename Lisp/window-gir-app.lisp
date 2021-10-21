9089ac53a5e175f2e676cf56c26c20bad35cba1e
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2021-10-21 10:26:39 +0100

    another find

--- Lisp/window-gir-app.lisp
+++ Lisp/window-gir-app.lisp
@@ -29,6 +29,7 @@
 ;; (gir-gtk  (last (parse-xml-file (path-gir-gtk))) (list 0 1587 9 3))
 ;; (gir-gtk  (last (parse-xml-file (path-gir-gtk))) (list 0 1587 9 3 '?))
 ;; (gir-gtk  (last (parse-xml-file (path-gir-gtk))) (list 0 'c))
+;; (gir-gtk  (last (parse-xml-file (path-gir-gtk))) (list 0 2630 86))
 (defun gir-gtk (lst indexes)
   (cond ((atom lst)
          (list 'last-atom
