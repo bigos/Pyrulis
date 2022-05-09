347d35add86c086fd85bde29493b27d994607540
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2022-05-09 15:01:09 +0100

    renaming

--- Lisp/tris/tris.lisp
+++ Lisp/tris/tris.lisp
@@ -3,28 +3,28 @@
 (in-package #:tris)
 
 (defstruct tris
-  (trib)
   (tria)
-  (trid))
+  (trib)
+  (tric))
 
-(defun tris (trib tria trid)
-  "Three way version of cons, with TRIA and TRID resembling CAR and CDR."
-  (make-tris :trib trib :tria tria :trid trid))
-
-(defun trib (tris)
-  (tris-trib tris))
-(defun (setf trib) (val tris)
-  (setf (tris-trib tris) val))
+(defun tris (tria trib tric)
+  "Three way version of cons, with TRIB and TRIC resembling CAR and CDR."
+  (make-tris :tria tria :trib trib :tric tric))
 
 (defun tria (tris)
   (tris-tria tris))
 (defun (setf tria) (val tris)
   (setf (tris-tria tris) val))
 
-(defun trid (tris)
-  (tris-trid tris))
-(defun (setf trid) (val tris)
-  (setf (tris-trid tris) val))
+(defun trib (tris)
+  (tris-trib tris))
+(defun (setf trib) (val tris)
+  (setf (tris-trib tris) val))
+
+(defun tric (tris)
+  (tris-tric tris))
+(defun (setf tric) (val tris)
+  (setf (tris-tric tris) val))
 
 (defun trilist (&rest values)
   (let ((head nil)
@@ -34,11 +34,11 @@
              (let ((current (tris b v nil)))
                (unless head (setf head current))
                (when b
-                 (setf (trid b) current))
+                 (setf (tric b) current))
                (setf b current)))
     head))
 
 (defun trilist-last (tris)
-  (if (trid tris)
-      (trilist-last (trid tris))
+  (if (tric tris)
+      (trilist-last (tric tris))
       tris))
