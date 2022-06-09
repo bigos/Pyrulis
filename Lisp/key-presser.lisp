c6ff58f41293bb0b4f1358aec08a1fc9f86d5d32
Jacek Podkanski <ruby.object@googlemail.com>
2022-06-09 19:43:53 +0100

    simulating key press

--- Lisp/key-presser.lisp
+++ Lisp/key-presser.lisp
@@ -10,3 +10,62 @@
   (:use #:cl))
 ;; (load "/home/jacek/Programming/Pyrulis/Lisp/key-presser.lisp")
 (in-package :key-presser)
+
+;; #####################################################################
+
+(defun key-press-from-name (name &optional modifiers)
+  "For testing, build needed key press data and add Shift for some NAMEs."
+
+  (list
+   ;;sanitised modifiers
+   (let ((first-char (aref name 0)))
+     (remove-if                   ;remove shift for lower case letters
+      (lambda (m) (and (eq m :shift)
+                       (lower-case-p first-char)))
+      (remove-duplicates
+       (remove-if-not                   ;remove invalid modifiers
+        (lambda (m) (member m '(:shift :ctrl :alt :super)))
+        (if (and (eq (length name) 1)
+                 (upper-case-p first-char)) ;add :shift for upper case letters
+            (cons  :shift
+                   modifiers)
+            modifiers)))))
+
+   ;; key character or "" for special keys
+   (let ((c (cond ((member name (list "Return" "Backspace" "Tab" "Escape" "Delete")
+                           :test #'equal)
+                   (name-char name))
+                  ;; we need more names in the list and better way of handling it
+                  ((member name (list "Insert" "Up" "Down" "Left" "Right" "Home"
+                                      "End" "Page_Down" "Page_Up")
+                           :test #'equal)
+                   "")
+                  ((member name (list "space")
+                           :test #'equal)
+                   " ")
+                  ((and (equal (subseq name 0 1) "F") ; F1-F12 ?
+                        (>= (length name) 2)
+                        (parse-integer name :start 1 :junk-allowed nil))
+                   "")
+                  ((eql 1 (length name))
+                   name)
+                  (t
+                   (error "Name ~S is not valid" name)))))
+
+     (if (stringp c)
+         c
+         (format nil "~A" c)))
+
+   ;; character name
+   name))
+
+(defun my-key-from-name (name &optional modifiers)
+  (let ((kp (key-press-from-name name modifiers)))
+    (make-my-key :modifiers (elt kp 0) :string (elt kp 1) :name (elt kp 2))))
+
+(defstruct my-key
+  (modifiers)
+  (string)
+  (name))
+
+;; #####################################################################
