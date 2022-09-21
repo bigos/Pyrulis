a3be408c9c8f8c3a7bd723669d2344c79e1dea13
Jacek Podkanski <ruby.object@googlemail.com>
2022-09-21 01:44:15 +0100

    better adding

--- Lisp/hashpath.lisp
+++ Lisp/hashpath.lisp
@@ -10,15 +10,22 @@
 ;; (load "~/Programming/Pyrulis/Lisp/hashpath.lisp")
 (in-package #:hashpath)
 
-(defun make-hashpath-table (key)
-  (let ((hash (make-hash-table)))
-    (setf (gethash :. hash) key)
-    hash))
-
 (defun hash-add (hash key value)
   (when (typep value 'hash-table)
-    (setf (gethash :.. value) hash))
+    (setf (gethash :.. value) hash
+          (gethash :.  value) key))
   (setf (gethash key hash) value))
+
+;; (hash-add-path zzz '('q :a :b :c) 3)
+(defun hash-add-path (hash keys value)
+  (loop for k in keys
+        for oldh = nil then h
+        for h = hash then (alexandria:ensure-gethash k h (make-hash-table))
+        do
+           (when oldh
+             (hash-add oldh k h))
+           (format t "~& printing ~S~%" h)
+        finally (setf (gethash k h) value)))
 
 (defun hashpath-tablep (current-hash key)
   (let ((hash (gethash key current-hash)))
@@ -27,8 +34,6 @@
          (typep (gethash :. hash) 'keyword)
          (not (null (gethash :.. hash)))
          (typep (gethash :.. hash) 'hash-table))))
-
-
 
 (-> hash-parent (hash-table) hash-table)
 (defun hash-parent (current-hash)
@@ -46,15 +51,23 @@
     (t
      (gethash key current-hash))))
 
+(defun parent-hash-table-alist (table)
+  "Returns an association list containing the keys and values of hash table
+TABLE replacing parent table with 'parent."
+  (let ((alist nil))
+    (maphash (lambda (k v)
+               (push (cons k
+                           (if (eql k :..)
+                               'parent
+                               v))
+                     alist))
+             table)
+    alist))
+
 (defmethod print-object ((obj hash-table)  stream)
   (print-unreadable-object (obj stream :type t)
     (format stream "~S"
-            (loop for k being each hash-key in obj
-                  collect (list k
-                                '=
-                                (if (eql k :..)
-                                    'parent
-                                    (gethash k zzz))) ))))
+          (parent-hash-table-alist obj))))
 
 (defun test-me ()
   (format t "~&Testing hashpath~%")
