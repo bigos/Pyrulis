f6cbcfa37601717cb17baa5e4fe138ea397aa640
Jacek Podkanski <ruby.object@googlemail.com>
2022-11-14 00:08:10 +0000

    view

--- Lisp/commanding.lisp
+++ Lisp/commanding.lisp
@@ -7,24 +7,34 @@
 ;; (load "~/Programming/Pyrulis/Lisp/commanding.lisp")
 (in-package #:commanding)
 
+(defparameter *model* nil)
+
+
 (defun prompt (prompt)
   (format t "~&~a > " prompt)
   (read-line))
 
-(defun coms ()
-  (list "quit" "help"))
+(defun coms () (list "quit" "help"))
 
 (defun command (input)
   (format t "commanding ~S~%" input)
-  (cond
-    ((and (equal input "quit")
-          (car (member "quit" (coms) :test #'equal)))
-     (error "should not end here because of quitting in the main loop"))
-    ((and (equal input "help")        ;figure out ensuring correctness
-          (car (member "help" (coms) :test #'equal)))
-     (format t "commands are ~S~%" (list "quit" "help")))
-    (T
-     (error "unrecognised command ~S~%" input))))
+  (labels ((is (com)
+             (and (equal input com)
+                  (car (member com (coms) :test #'equal)))))
+    (cond
+      ((is "quit")
+       (error "should not end here because of quitting in the main loop"))
+      ((is "help")
+       (format t "commands are ~S~%" (list "quit" "help")))
+      (T
+       (error "unrecognised command ~S~%" input))))
+  (view))
+
+(defun view ()
+  (format t "~&starting view ====++============~%")
+
+  (format t "~&finished view ==================~%")
+  nil)
 
 (defun main ()
   (loop for input = (prompt "enter command")
