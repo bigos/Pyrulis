69661307cb9a56b2a65f8d5c0f08b805f411ec15
Jacek Podkanski <ruby.object@googlemail.com>
2022-11-15 07:37:08 +0000

    some reading

--- Elm/readingElmInAction/PhotoGroove/src/PhotoGroove.elm
+++ Elm/readingElmInAction/PhotoGroove/src/PhotoGroove.elm
@@ -10,7 +10,14 @@ import Html.Events exposing (onClick)
 type alias Model =
     { photos : List Photo
     , selectedUrl : String
+    , chosenSize : Thumbnailsize
     }
+
+
+type Thumbnailsize
+    = Small
+    | Medium
+    | Large
 
 
 type alias Photo =
@@ -21,12 +28,17 @@ type alias Msg =
     { description : String, data : String }
 
 
+update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "Clicked photo" then
-        { model | selectedUrl = msg.data }
+    case msg.description of
+        "Clicked photo" ->
+            { model | selectedUrl = msg.data }
 
-    else
-        model
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
 
 
 photoListUrl : String
@@ -42,10 +54,34 @@ view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
         , div [ id "thumbnails" ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
         ]
+
+
+viewSizeChooser : Thumbnailsize -> Html Msg
+viewSizeChooser size =
+    label [] [ input [ type_ "radio", name "size" ] [], text (sizeToString size) ]
+
+
+sizeToString : Thumbnailsize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "medium"
+
+        Large ->
+            "large"
 
 
 viewThumbnail : String -> Photo -> Html Msg
@@ -63,7 +99,7 @@ photoArray =
     Array.fromList initialModel.photos
 
 
-initialModel : { photos : List Photo, selectedUrl : String }
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -71,6 +107,7 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
     }
 
 
