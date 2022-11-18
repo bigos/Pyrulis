2d5b6f0ce77090e1a950fd3dee1d7692ce2af2c4
Jacek Podkanski <ruby.object@googlemail.com>
2022-11-18 23:58:17 +0000

    starting again

--- Lisp/commanding.lisp
+++ Lisp/commanding.lisp
@@ -14,58 +14,27 @@
   (format t "~&~a > " prompt)
   (read-line))
 
-(defun coms () (list "quit" "help" "inspect"))
-
 #| command succession
   repl_input
-runtime
+  runtime
   command
   model
   view
-runtime
+  runtime
   repl_print
 |#
 
 (defclass/std runtime () ())
 
-(defun init ()
-  (setf *model* nil))
 
-(defun command (input)
-  (format t "commanding ~S~%" input)
-  (labels ((is (com)
-             (and (equal input com)
-                  (car (member com (coms) :test #'equal)))))
-    (cond
-      ((is "quit")
-       (setf  (getf *model* :mode) :quit))
-      ((is "help")
-       (setf (getf *model* :mode) :help))
-      ((is "inspect")
-       (setf (getf *model* :mode) :inspect))
-      (T
-       (error "unrecognised command ~S~%" input))))
-  (view))
-
-(defun view ()
-  (format t "~&starting view ====++============~%")
-  (let ((m (getf *model* :mode)))
-    (ecase m
-      (:quit (format t "quitting~%"))
-      (:help (format t "the commands are ~S~%" (coms)))
-      (:inspect (format t "model is: ~S~%" *model*))
-      ))
-
-  (format t "~&finished view ==================~%")
-  nil)
 
 (defun main ()
-  (init)
+  ;; (init)
 
   (loop for input = (prompt "enter command")
         do
            (format t "you have said ~S~%" input)
-           (command input)
-        until (eq :quit (getf *model* :mode))
+           ;; (command input)
+        until (equal input "quit")
         finally
            (format t "quitting~%")))
