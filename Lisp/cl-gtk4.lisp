599b380e84fce5c4032d8b22504d1c616ba08f82
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2022-12-20 14:45:44 +0000

    simplest counter

---
+++ Lisp/cl-gtk4.lisp
@@ -0,0 +1,36 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload '(cl-gtk4) ))
+
+(defpackage cl-gtk4
+  (:use #:cl #:gtk4))
+
+;; (load "~/Programming/Pyrulis/Lisp/cl-gtk4.lisp")
+(in-package #:cl-gtk4)
+
+(defun simple ()
+  (let ((app (make-application :application-id "org.bohonghuang.cl-gtk4-example"
+                               :flags gio:+application-flags-flags-none+)))
+    (connect app "activate"
+             (lambda (app)
+               (let ((window (make-application-window :application app)))
+                 (setf (window-title window) "Simple Counter")
+                 (let ((box (make-box :orientation +orientation-vertical+
+                                      :spacing 4)))
+                   (let ((label (make-label :str "0")))
+                     (setf (widget-hexpand-p label) t
+                           (widget-vexpand-p label) t)
+                     (box-append box label)
+                     (let ((button (make-button :label "Add"))
+                           (count 0))
+                       (connect button "clicked" (lambda (button)
+                                                   (declare (ignore button))
+                                                   (setf (label-text label) (format nil "~A" (incf count)))))
+                       (box-append box button))
+                     (let ((button (make-button :label "Exit")))
+                       (connect button "clicked" (lambda (button)
+                                                   (declare (ignore button))
+                                                   (window-destroy window)))
+                       (box-append box button)))
+                   (setf (window-child window) box))
+                 (window-present window))))
+    (gio:application-run app nil)))
