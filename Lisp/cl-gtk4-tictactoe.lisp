406f145bc6dc6c4cff8cffa16e6a03598c97168d
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-01-20 16:24:02 +0000

    trying menu

--- Lisp/cl-gtk4-tictactoe.lisp
+++ Lisp/cl-gtk4-tictactoe.lisp
@@ -680,21 +680,28 @@
                             (menu-item-quit (gio:make-menu-item :label "Quit"
                                                                 :detailed-action "app.quit" )))
 
-
                        (gio:menu-append-item menu menu-item-quit)
-                       (gobject:object-unref menu-item-quit)
 
                        (setf
                         (gio:menu-item-submenu menu-item-menu) menu)
 
                        (gio:menu-append-item menubar menu-item-menu)
-                       (gobject:object-unref menu-item-menu)
 
                        (setf
                         (gtk4:application-menubar app) menubar
                         (gtk4:application-window-show-menubar-p window) T)
-                       (window-present window))))))
 
+                       (window-present window)
+
+                       (format t "past window present ~%")
+
+                       ;; (gobject:object-unref menubar)
+                       ;; (gobject:object-unref menu-item-menu)
+                       ;; (gobject:object-unref menu)
+                       ;; (gobject:object-unref menu-item-quit)
+                       )
+                     (gobject:object-unref act-quit)))))
+      ;; https://stackoverflow.com/questions/69135934/creating-a-simple-menubar-menu-and-menu-item-in-c-using-gtk4
       (setf stat (gio:application-run app nil))
       (format t "~S~%" *model*)
       (gobject:object-unref app))
