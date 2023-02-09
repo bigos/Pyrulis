370e3f84ecd5e412166eacd9893412888dc29a34
Jacek Podkanski <ruby.object@googlemail.com>
2023-02-09 23:11:00 +0000

    ormulised haskell

--- Haskell/average_number.hs
+++ Haskell/average_number.hs
@@ -2,21 +2,23 @@
 
 module Main where
 
-import Data.List
 import Data.Function
+import Data.List
 
 flodiv a b = (fromIntegral a) / (fromIntegral b)
 
-strToInts s = map (\n -> read n :: Integer)  (words s)
+strToInts s = map (\n -> read n :: Integer) (words s)
 
 sol nn =
-  show (if final == []
-        then ( (-1))
-        else ( floor (head final)))
+  show
+    ( if final == []
+        then ((-1))
+        else (floor (head final))
+    )
   where
-    n  = head nn !! 0
-    k  = head nn !! 1
-    v  = head nn !! 2
+    n = head nn !! 0
+    k = head nn !! 1
+    v = head nn !! 2
     ax = nn !! 1
     spil = n + k
     sumr = sum ax
@@ -24,7 +26,7 @@ sol nn =
     avl = (n + k)
     formula x = flodiv (sumn x) avl
     formulas = (map formula [0 .. spil])
-    final = (filter (\a-> a  == (fromInteger v)) formulas)
+    final = (filter (\a -> a == (fromInteger v)) formulas)
 
 solution :: String -> [String]
 solution a =
@@ -32,8 +34,8 @@ solution a =
   where
     ll = lines a
     t = head (strToInts (head ll))
-    si n e = strToInts (ll !! (e*2+n))
+    si n e = strToInts (ll !! (e * 2 + n))
     endv = (fromIntegral (t - 1))
-    tcx = map (\e -> sol [si 1 e , si 2 e]) [0 ..  endv]
+    tcx = map (\e -> sol [si 1 e, si 2 e]) [0 .. endv]
 
 main = getContents >>= mapM putStrLn . solution
