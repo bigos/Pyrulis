58c90a03cbf8dab0e3305ddc4394ea78f3632a1b
Jacek Podkanski <ruby.object@googlemail.com>
2023-02-17 23:34:22 +0000

    progressing to different way of creating test data

--- Elm/readingElmInAction/PhotoGroove/tests/PhotoGrooveTests.elm
+++ Elm/readingElmInAction/PhotoGroove/tests/PhotoGrooveTests.elm
@@ -2,7 +2,7 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Json.Decode as Decode exposing (decodeString)
+import Json.Decode as Decode exposing (decodeString, decodeValue)
 import Json.Encode as Encode
 import PhotoGroove
 import Test exposing (..)
@@ -12,8 +12,9 @@ decoderTest : Test
 decoderTest =
     test "title defaults to (untitled)" <|
         \_ ->
-            """{"url": "fruits.com", "size":5}"""
-                |> decodeString PhotoGroove.photoDecoder
+            -- """{"url": "fruits.com", "size":5}"""
+            Encode.object [ ( "url", Encode.string "fruits .com" ), ( "size", Encode.int 5 ) ]
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal
                     (Ok "(untitled)")
