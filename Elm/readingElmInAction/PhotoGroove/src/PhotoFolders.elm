04307ff49c199aed3fd45497af6a60940d113837
Jacek Podkanski <ruby.object@googlemail.com>
2023-02-24 23:44:31 +0000

    photo folders skeleton

---
+++ Elm/readingElmInAction/PhotoGroove/src/PhotoFolders.elm
@@ -0,0 +1,66 @@
+module PhotoFolders exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Model =
+    { selectPhotoUrl : Maybe String }
+
+
+initialModel : Model
+initialModel =
+    { selectPhotoUrl = Nothing }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel
+    , Http.get
+        { url = "http://elm-in-action.com/folders/list"
+        , expect = Http.expectJson GotInitialModel modelDecoder
+        }
+    )
+
+
+modelDecoder : Decoder Model
+modelDecoder =
+    Decode.succeed initialModel
+
+
+type Msg
+    = ClickedPhoto String
+    | GotInitialModel (Result Http.Error Model)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedPhoto url ->
+            ( { model | selectPhotoUrl = Just url }, Cmd.none )
+
+        GotInitialModel (Ok newModel) ->
+            ( newModel, Cmd.none )
+
+        GotInitialModel (Err _) ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text "The Grooviest Folders the world has ever seen" ]
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
