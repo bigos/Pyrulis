c34ccdf775c8ea3e007f6a173fd397dc585d423a
Jacek Podkanski <ruby.object@googlemail.com>
2023-02-24 16:09:53 +0000

    beginning to test views

--- Elm/readingElmInAction/PhotoGroove/src/PhotoGroove.elm
+++ Elm/readingElmInAction/PhotoGroove/src/PhotoGroove.elm
@@ -1,4 +1,15 @@
-port module PhotoGroove exposing (Model, Msg(..), Photo, initialModel, main, photoDecoder, update)
+port module PhotoGroove exposing
+    ( Model
+    , Msg(..)
+    , Photo
+    , Status(..)
+    , initialModel
+    , main
+    , photoDecoder
+    , update
+    , urlPrefix
+    , view
+    )
 
 import Array exposing (Array)
 import Browser
