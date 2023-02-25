0ab88faa3d9d15185b0ca3cdc7cee7131cc4fb2c
Jacek Podkanski <ruby.object@googlemail.com>
2023-02-25 16:48:53 +0000

    preparing code for the new page

--- Elm/readingElmInAction/PhotoGroove/src/PhotoFolders.elm
+++ Elm/readingElmInAction/PhotoGroove/src/PhotoFolders.elm
@@ -64,3 +64,38 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src (urlPrefix ++ "photos/" ++ photo.url ++ "/full") ] []
+        , span [] [ text (String.fromInt photo.size ++ "KB") ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ]
+            (List.map viewRelatedPhoto photo.relatedUrls)
+        ]
+
+
+viewRelatedPhoto : String -> Html Msg
+viewRelatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (ClickedPhoto url)
+        , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
+        ]
+        []
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com"
