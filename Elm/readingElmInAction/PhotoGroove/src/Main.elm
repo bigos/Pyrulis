ed2045c170c80596778799e5d948d9ef75c2afc7
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-02-28 13:56:25 +0000

    showed the first page

--- Elm/readingElmInAction/PhotoGroove/src/Main.elm
+++ Elm/readingElmInAction/PhotoGroove/src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser exposing (Document)
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
-import Html.Attributes exposing (classlist, href)
+import Html.Attributes exposing (classList, href)
 
 
 type alias Model =
