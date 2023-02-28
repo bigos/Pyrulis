b43f4fecfb920b8dd62e8f2433fbed33522d4035
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-02-28 14:33:56 +0000

    header footer and lazy optimisation of the header

--- Elm/readingElmInAction/PhotoGroove/src/Main.elm
+++ Elm/readingElmInAction/PhotoGroove/src/Main.elm
@@ -3,19 +3,57 @@ module Main exposing (main)
 import Browser exposing (Document)
 import Html exposing (Html, a, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
 
 
 type alias Model =
-    {}
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
 
 
 view : Model -> Document Msg
 view model =
+    let
+        content =
+            text "This isn't even my final form!"
+    in
     { title = "Photo Groove, SPA Style"
     , body =
-        [ text "This isn't even my final form!"
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
         ]
     }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "PhotoGroove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
 
 
 type Msg
@@ -35,7 +73,7 @@ subscriptions model =
 main : Program () Model Msg
 main =
     Browser.document
-        { init = \_ -> ( {}, Cmd.none )
+        { init = \_ -> ( { page = Gallery }, Cmd.none )
         , subscriptions = subscriptions
         , update = update
         , view = view
