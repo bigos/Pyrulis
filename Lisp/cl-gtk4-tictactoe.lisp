4944d399a5181aa4b08b2df13ed0d022b9eee594
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-03-03 15:01:28 +0000

    this did not work

--- Lisp/cl-gtk4-tictactoe.lisp
+++ Lisp/cl-gtk4-tictactoe.lisp
@@ -654,8 +654,16 @@
                                                         "activate"
                                                         event
                                                         args)
+
                                                        ;; this quits the app by closing the window
-                                                       (gtk4:window-close window)))))
+                                                       (let ((windows (gtk4:window-list-toplevels)))
+                                                         (if (atom windows)
+                                                             (progn
+                                                               (format t "values for closing: window ~S  windowS ~S~%" window windows)
+                                                               (gtk4:window-close window))
+                                                             (progn
+                                                               (format t "finish me for closing multiple windows~%")
+                                                               (warn "closing of multiple windows not implemented"))))))))
          (menubar-item-help (gio:make-menu-item :label "Help" :detailed-action nil))
          (help (gio:make-menu))
          (help-item-manual (gio:make-menu-item :label "Manual"
