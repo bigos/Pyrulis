18cab64233e870e92bf58ff7412741e3121bdb73
Jacek Podkanski <ruby.object@googlemail.com>
2023-06-19 21:47:53 +0100

    playing with control monad

--- Haskell/prompt.hs
+++ Haskell/prompt.hs
@@ -1,9 +1,12 @@
 module Main where
 
+import Control.Applicative
+import Control.Monad
+
 -- doesn't work in Emacs, neither intero nor the old REPL work
 
 main :: IO ()
 main =
-  putStrLn "Type something" >>
-  getLine >>=
-    (\line -> putStrLn ("you have typed: " ++ line))
+  putStrLn "Type something"
+    >> getLine
+    >>= (\line -> putStrLn ("you have typed: " ++ line))
