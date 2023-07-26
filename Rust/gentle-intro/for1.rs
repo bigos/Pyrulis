9282dc03df379f8852dcef10f9d67d7218027947
Jacek Podkanski <ruby.object@googlemail.com>
2023-07-26 22:52:00 +0100

    more examples

---
+++ Rust/gentle-intro/for1.rs
@@ -0,0 +1,7 @@
+// for1.rs
+
+fn main() {
+    for i in 0..5 {
+        println!("Hello {}", i);
+    }
+}
---
+++ Rust/gentle-intro/let2.rs
@@ -0,0 +1,6 @@
+// let2.rs
+
+fn main() {
+    let answer = 42;
+    assert_eq!(answer, 4);
+}
