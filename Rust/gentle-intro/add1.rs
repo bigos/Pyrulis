e7eb469e5f415f0c680f9ecaafd5000b24c88516
Jacek Podkanski <ruby.object@googlemail.com>
2023-07-26 23:46:59 +0100

    exercises

---
+++ Rust/gentle-intro/add1.rs
@@ -0,0 +1,8 @@
+// add1.rs
+fn main() {
+    let mut sum = 0;
+    for _i in 0..5 {
+        sum += 1;
+    }
+    println!("sum is {}", sum);
+}
---
+++ Rust/gentle-intro/for2.rs
@@ -0,0 +1,11 @@
+// for2.rs
+
+fn main() {
+    for i in 0..5 {
+        if i % 2 == 0 {
+            println!("even {}", i);
+        } else {
+            println!("odd {}", i);
+        }
+    }
+}
