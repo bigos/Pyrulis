0d4c698d793f23125681a2ce8386d8a4c8d5802d
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-07-27 16:49:54 +0100

    vec1.rs TODO

--- Rust/gentle-intro/slice1.rs
+++ Rust/gentle-intro/slice1.rs
@@ -1,3 +1,10 @@
 // slice1.rs
 
-TODO
+fn main() {
+    let ints = [1, 2, 3, 4, 5];
+    let slice1 = &ints[0..2];
+    let slice2 = &ints[1..]; // open range
+    println!("ints {:?}", ints);
+    println!("slice1 {:?}", slice1);
+    println!("slice2 {:?}", slice2);
+}
---
+++ Rust/gentle-intro/vec1.rs
@@ -0,0 +1,3 @@
+// vec1.rs
+
+TODO
