c8f81a9fd7a93f77ac2f5e798ce45ec76407345d
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-22 00:47:36 +0100

    possibly giving up after the first experiment

--- OCaml/experiments/lispen/_build/default/bin/main.ml
+++ OCaml/experiments/lispen/_build/default/bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+let skip = print_endline "skipping"
+
+let compile = print_endline "compile"
+
+let main =
+  (* flush the terminal *)
+  Printf.printf "%!" ;
+  Printf.printf "\nEmacs -------------------------------\n\n" ;
+  Sys.chdir "/home/jacek/Programming/emacs-31" ;
+  let _ = Sys.command "git pull" in
+  Printf.printf "Should I compile Emacs? Please enter your choice Y/n > " ;
+  let rl = Stdlib.read_line () |> String.trim in
+  if rl = "Y" then compile else if rl = "y" then compile else skip
+
+let () = main
