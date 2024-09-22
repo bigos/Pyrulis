d8515f4ff2ce97242c0194ccb5144e1ebc2d4c2f
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-22 09:42:41 +0100

    slight progress

--- OCaml/experiments/lispen/bin/main.ml
+++ OCaml/experiments/lispen/bin/main.ml
@@ -1,6 +1,8 @@
 let skip name = Printf.printf "skipping %s" name
 
-let compile () = print_endline "compile"
+let compileEmacs () = print_endline "compile Emscs"
+
+let compileSBCL () = print_endline "compile SBCL"
 
 let main () =
   Printf.printf "\nEmacs -------------------------------\n\n" ;
@@ -8,15 +10,17 @@ let main () =
   let _ = Sys.command "git pull" in
   Printf.printf "Should I compile Emacs? Please enter your choice Y/n > " ;
   let rl = Stdlib.read_line () |> String.trim in
-  if rl = "Y" then compile () else if rl = "y" then compile () else skip "Emacs" ;
+  if rl = "Y" then compileEmacs ()
+  else if rl = "y" then compileEmacs ()
+  else skip "Emacs" ;
   (* sbcl *)
   Printf.printf "\nSBCL -------------------------------\n\n" ;
   Sys.chdir "/home/jacek/Programming/sbcl" ;
   let _ = Sys.command "git pull" in
   Printf.printf "Should I compile SBCL? Please enter your choice Y/n > " ;
   let rls = Stdlib.read_line () |> String.trim in
-  if rls = "Y" then compile ()
-  else if rls = "y" then compile ()
+  if rls = "Y" then compileSBCL ()
+  else if rls = "y" then compileSBCL ()
   else skip "SBCL" ;
   Printf.printf "\n"
 
