03990abd2ef6845f60cc7b6ffd19990906558c37
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-22 13:41:48 +0100

    thinking about compiling

--- OCaml/experiments/lispen/bin/main.ml
+++ OCaml/experiments/lispen/bin/main.ml
@@ -1,27 +1,37 @@
 let skip name = Printf.printf "skipping %s" name
 
-let compileEmacs () = print_endline "compile Emscs"
+let printHeader name =
+  Printf.printf "\n--- %s -------------------------------\n\n" name
 
-let compileSBCL () = print_endline "compile SBCL"
+let compileEmacs () =
+  print_endline "compile Emscs" ;
+  Sys.command "make; sudo make install"
 
-let main () =
-  Printf.printf "\nEmacs -------------------------------\n\n" ;
+let compileSBCL () =
+  print_endline "compile SBCL" ;
+  Sys.command "sh./clean.sh; sh /make.sh; sudo sh ./install "
+
+let doEmacs () =
+  printHeader "Emacs" ;
   Sys.chdir "/home/jacek/Programming/emacs-31" ;
-  let _ = Sys.command "git pull" in
+  let _ = Sys.command "git pull; echo 'pulled Emacs'" in
   Printf.printf "Should I compile Emacs? Please enter your choice Y/n > " ;
   let rl = Stdlib.read_line () |> String.trim in
   if rl = "Y" then compileEmacs ()
   else if rl = "y" then compileEmacs ()
-  else skip "Emacs" ;
-  (* sbcl *)
-  Printf.printf "\nSBCL -------------------------------\n\n" ;
+  else skip "Emacs"
+
+let doSBCL () =
+  printHeader "SBCL" ;
   Sys.chdir "/home/jacek/Programming/sbcl" ;
-  let _ = Sys.command "git pull" in
+  let _ = Sys.command "git pull; echo 'pulled SBCL'" in
   Printf.printf "Should I compile SBCL? Please enter your choice Y/n > " ;
   let rls = Stdlib.read_line () |> String.trim in
   if rls = "Y" then compileSBCL ()
   else if rls = "y" then compileSBCL ()
   else skip "SBCL" ;
   Printf.printf "\n"
+
+let main () = doEmacs () ; doSBCL ()
 
 let () = main ()
