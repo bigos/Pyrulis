3fac857b157f30b642f568b7543ea09444057240
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-22 09:39:43 +0100

    skipping with parameters

--- OCaml/experiments/lispen/bin/main.ml
+++ OCaml/experiments/lispen/bin/main.ml
@@ -1,4 +1,4 @@
-let skip () = print_endline "skipping"
+let skip name = Printf.printf "skipping %s" name
 
 let compile () = print_endline "compile"
 
@@ -8,6 +8,16 @@ let main () =
   let _ = Sys.command "git pull" in
   Printf.printf "Should I compile Emacs? Please enter your choice Y/n > " ;
   let rl = Stdlib.read_line () |> String.trim in
-  if rl = "Y" then compile () else if rl = "y" then compile () else skip ()
+  if rl = "Y" then compile () else if rl = "y" then compile () else skip "Emacs" ;
+  (* sbcl *)
+  Printf.printf "\nSBCL -------------------------------\n\n" ;
+  Sys.chdir "/home/jacek/Programming/sbcl" ;
+  let _ = Sys.command "git pull" in
+  Printf.printf "Should I compile SBCL? Please enter your choice Y/n > " ;
+  let rls = Stdlib.read_line () |> String.trim in
+  if rls = "Y" then compile ()
+  else if rls = "y" then compile ()
+  else skip "SBCL" ;
+  Printf.printf "\n"
 
 let () = main ()
