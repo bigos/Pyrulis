516dc8f02dfee8016405ded0fe180d9ac344bb35
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-22 15:01:47 +0100

    trying to run, but with wrong order

--- OCaml/experiments/lispen/bin/main.ml
+++ OCaml/experiments/lispen/bin/main.ml
@@ -1,37 +1,42 @@
-let skip name = Printf.printf "skipping %s" name
+let skip name =
+  let _ = Sys.command "echo 'skipped'" in
+  Printf.printf "skipping %s" name
+;;
 
-let printHeader name =
-  Printf.printf "\n--- %s -------------------------------\n\n" name
+let printHeader name = Printf.printf "\n--- %s -------------------------------\n\n" name
 
 let compileEmacs () =
-  print_endline "compile Emscs" ;
-  Sys.command "make; sudo make install"
+  let _ = Sys.command "make; sudo make install" in
+  Printf.printf "compile Emacs"
+;;
 
 let compileSBCL () =
-  print_endline "compile SBCL" ;
-  Sys.command "sh./clean.sh; sh /make.sh; sudo sh ./install "
+  let _ = Sys.command "sh ./distclean.sh; sh ./make.sh; sudo sh ./install.sh" in
+  Printf.printf "compile SBCL"
+;;
 
 let doEmacs () =
-  printHeader "Emacs" ;
-  Sys.chdir "/home/jacek/Programming/emacs-31" ;
+  printHeader "Emacs";
+  Sys.chdir "/home/jacek/Programming/emacs-31";
   let _ = Sys.command "git pull; echo 'pulled Emacs'" in
-  Printf.printf "Should I compile Emacs? Please enter your choice Y/n > " ;
+  Printf.printf "Should I compile Emacs? Please enter your choice Y/n > ";
   let rl = Stdlib.read_line () |> String.trim in
-  if rl = "Y" then compileEmacs ()
-  else if rl = "y" then compileEmacs ()
-  else skip "Emacs"
+  if rl = "Y" || rl = "y" then compileEmacs () else skip "Emacs"
+;;
 
-let doSBCL () =
-  printHeader "SBCL" ;
-  Sys.chdir "/home/jacek/Programming/sbcl" ;
+let doSbcl () =
+  printHeader "SBCL";
+  Sys.chdir "/home/jacek/Programming/sbcl";
   let _ = Sys.command "git pull; echo 'pulled SBCL'" in
-  Printf.printf "Should I compile SBCL? Please enter your choice Y/n > " ;
-  let rls = Stdlib.read_line () |> String.trim in
-  if rls = "Y" then compileSBCL ()
-  else if rls = "y" then compileSBCL ()
-  else skip "SBCL" ;
+  Printf.printf "Should I compile SBCL? Please enter your choice Y/n > ";
+  let rl = Stdlib.read_line () |> String.trim in
+  if rl = "Y" || rl = "y" then compileSBCL () else skip "SBCL";
   Printf.printf "\n"
+;;
 
-let main () = doEmacs () ; doSBCL ()
+let main () =
+  doEmacs ();
+  doSbcl ()
+;;
 
 let () = main ()
