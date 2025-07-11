8017caf68f97a56e5559fc6d3c2b68d3f6b3c23b
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-11 18:31:42 +0100

    first counter was styled

--- Purescript/halogen/counter/src/App/Counter.purs
+++ Purescript/halogen/counter/src/App/Counter.purs
@@ -1,15 +1,25 @@
 module App.Counter where
 
 import Prelude
+
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 
 type State
   = { count :: Int }
 
 data Action
-  = Increment
+  = Increment | Decrement
+
+counter_color :: Int -> String
+counter_color count =
+  if count == 0
+  then "background: white"
+  else (if count < 0
+        then "background: red"
+        else "background: lime")
 
 component :: forall q i o m. H.Component q i o m
 component =
@@ -21,14 +31,31 @@ component =
 
 render :: forall cs m. State -> H.ComponentHTML Action cs m
 render state =
-  HH.div_
-    [ HH.p_
-        [ HH.text $ "You clicked " <> show state.count <> " times" ]
-    , HH.button
-        [ HE.onClick \_ -> Increment ]
-        [ HH.text "Click me" ]
+  HH.div [HP.style
+          (
+            "display: inline-flex;" <>
+            "margin: 1em;" <>
+            "padding:1em;" <>
+            "background: yellow;"
+          )
+         ,
+          HP.class_ (HH.ClassName  "first-class")
+         ]
+  [HH.button
+   [HE.onClick \_ -> Decrement]
+   [HH.text "-"]
+  , HH.p [HP.style "background: white; padding: 1em"]
+    [HH.text "You counted "
+    , HH.span [HP.style ("padding: 0.25em; " <> (counter_color state.count))]
+      [HH.text (show state.count)]
+    , HH.text " times"
     ]
+  , HH.button
+    [HE.onClick \_ -> Increment ]
+    [HH.text "+" ]
+  ]
 
 handleAction :: forall cs o m. Action → H.HalogenM State Action cs o m Unit
 handleAction = case _ of
   Increment -> H.modify_ \st -> st { count = st.count + 1 }
+  Decrement -> H.modify_ \st -> st { count = st.count - 1 }
