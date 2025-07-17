0d6ac9e546f967f68cb66720569cb19e3d6462c7
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-17 23:00:08 +0100

    imports and preparation to code flags

--- Purescript/halogen/counter/src/Main.purs
+++ Purescript/halogen/counter/src/Main.purs
@@ -4,14 +4,22 @@ import Prelude
 
 import App.Counter as Counter
 import Control.Monad.Error.Class (throwError)
+import Data.Maybe (Maybe(..))
 import Data.Maybe (maybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Console (log)
 import Effect.Exception (error)
+import Effect.Exception (throw)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Web.DOM.Element (getAttribute, id, toNode)
+import Web.DOM.NonElementParentNode (getElementById)
 import Web.DOM.ParentNode (QuerySelector(..))
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
 import Web.HTML.HTMLElement (HTMLElement)
+import Web.HTML.Window (document)
 
 -- this uses Elm like widget in a div, allowing me to mix PureScript with Rails generated views
 awaitElement :: Aff HTMLElement
@@ -22,6 +30,9 @@ awaitElement = do
   where
   elementName = "#halogen"
 
+-- data-my-app--api-endpoint="https://example.com/"
+-- data-my-app--api-key="0xdeadb33f"
+-- data-count-start="12"
 flagdata :: String
 flagdata = "name=user--apikey=123456"
 
@@ -30,3 +41,27 @@ main = HA.runHalogenAff do
   HA.awaitLoad
   element <- awaitElement
   runUI Counter.component flagdata element
+
+-- main :: Effect Unit
+-- main = do
+--   -- https://book.purescript.org/chapter8.html
+--   w <- window
+-- doc <- document w
+-- ctr <- getElementById "script_with_flags" $ toNonElementParentNode doc
+-- case ctr of
+--   Nothing ->
+--     throw "Container element not found."
+--   Just el ->
+--     do
+--       s <- getAttribute "id" el
+--       case s of
+--         Nothing ->
+--           throw "no id attribute"
+--         Just idAttr ->
+--           log (show idAttr)
+--       flags <- getAttribute "data-flags" el
+--       case flags of
+--         Nothing ->
+--           throw "no data-flags"
+--         Just flagsAttr ->
+--           log (show flagsAttr)
