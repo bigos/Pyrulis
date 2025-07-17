a9cfb498054b70867364d35b1ebf26bfc2c9a50a
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2025-07-17 15:04:32 +0100

    no luck playing with the code

--- Purescript/parameters-and-configuration/configured-project/src/Main.purs
+++ Purescript/parameters-and-configuration/configured-project/src/Main.purs
@@ -2,37 +2,46 @@ module Main where
 
 import Prelude
 
-import Data.Traversable
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Traversable (traverse)
+import Data.Tuple as Tuple
+
+import Effect.Console as Console
 import Effect (Effect)
 import Effect.Console (log)
+
 import Web.DOM.Element (getAttribute)
-import Web.HTML (window)
 import Web.HTML.HTMLDocument (currentScript)
 import Web.HTML.HTMLScriptElement as HTMLScript
 import Web.HTML.Window (Window, document)
+
+import Web.DOM.ParentNode (QuerySelector(..))
+import Web.HTML (window)
+import Web.HTML.Window (Window, localStorage)
+import Web.Storage.Storage (Storage, getItem, setItem)
 
 type TagInsertionConfig =
   { endpoint :: Maybe String
   , apiKey :: Maybe String
   }
 
---readConfig :: Window -> Effect TagInsertionConfig
-readConfig win = do
-  script <- currentScript =<< document win
+readConfig :: Effect TagInsertionConfig
+readConfig = do
+  w <- window
+  script <- currentScript =<< w
   traverse go script
   where
   go script = do
-    { endpoint: getAttribute "data-my-app--api-endpoint" elem
-    , apiKey: getAttribute "data-my-app--api-key" elem
-    }
-    where
-    elem = HTMLScript.toElement script
+    let elem = HTMLScript.toElement script
+    pure
+      { endpoint: getAttribute "data-my-app--api-endpoint" elem
+      , apiKey:
+          getAttribute "data-my-app--api-key" elem
+      }
 
 main :: Effect Unit
 main = do
-  w <- window
-  config <- readConfig w
+  config <- readConfig
   log "Hello, start adding some code."
   log (show config)
 
