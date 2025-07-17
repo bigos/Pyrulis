c898bc783f701924a609e8e8982411d8f3d11f66
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2025-07-17 15:57:20 +0100

    prepared the example

--- Purescript/parameters-and-configuration/second-attempt/spago.dhall
+++ Purescript/parameters-and-configuration/second-attempt/spago.dhall
@@ -1,4 +1,4 @@
-{ name = "my-new-project"
+{ name = "second-attempt"
 , dependencies = [ "console", "effect", "prelude" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- Purescript/parameters-and-configuration/second-attempt/src/Main.purs
+++ Purescript/parameters-and-configuration/second-attempt/src/Main.purs
@@ -1,10 +1,25 @@
 module Main where
 
 import Prelude
-
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Console (log)
+import Prelude
+import Web.DOM (querySelector, toDocument)
+import Web.HTML (window)
+import Web.HTML.Window (document)
+
+-- main :: Effect Unit
+-- main = do
+--   log "Hello, start adding some code."
 
 main :: Effect Unit
 main = do
-  log "Hello, start adding some code."
+  win <- window
+  doc <- document win
+  mEl <- querySelector "#script_with_flags" (toDocument doc)
+  case mEl of
+    Just el -> log "Found the element!"
+    Nothing -> log "No element found."
